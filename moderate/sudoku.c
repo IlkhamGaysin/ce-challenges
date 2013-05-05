4c4f5ac5fd0473a56534affa15f15791c5b12879
Nicolas Kaiser <nikai@nikai.net>
2013-05-05 18:01:39 +0200

    add Sudoku
    C

---
+++ moderate/sudoku.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int sq(int a) {
+    int ret = -1;
+    switch (a) {
+    case 4:
+	ret = 2;
+	break;
+    case 9:
+	ret = 3;
+	break;
+    }
+    return ret;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    int a;
+
+    fp = fopen(*++argv, "r");
+    while (fscanf(fp, "%d%*c", &a) != EOF) {
+	bool valid = true;
+	int d, i, j, crow;
+	int *csqu = calloc(sq(a), sizeof(int));
+	int *col = calloc(a, sizeof(int));
+
+	for (i = 0; i < a; i++) {
+	    crow = 0;
+	    for (j = 0; j < a; j++) {
+		fscanf(fp, "%d%*c", &d);
+		d = 1 << (d - 1);
+		crow |= d;
+		csqu[i % sq(a)] |= d;
+		col[i] |= d;
+	    }
+	    d = (1 << a) - 1;
+	    if (crow != d) {
+		valid = false;
+	    }
+	    if ((i + 1) % sq(a) == 0) {
+		for (j = 0; j < sq(a); j++) {
+		    if (csqu[j] != d) {
+			valid = false;
+		    } else {
+			csqu[j] = 0;
+		    }
+		}
+	    }
+	}
+	for (i = 0; valid && i < a; i++) {
+	    if (col[i] != (1 << a) - 1) {
+		valid = false;
+	    }
+	}
+	if (valid)
+	    printf("True\n");
+	else
+	    printf("False\n");
+	free(col);
+	free(csqu);
+    }
+    return 0;
+}
