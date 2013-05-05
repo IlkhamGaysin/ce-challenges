d6752bd76a0ab999d48cf9da51113d641e7de134
Nicolas Kaiser <nikai@nikai.net>
2013-05-05 21:49:04 +0200

    add Minimum Path Sum
    C

---
+++ hard/min_path_sum.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min(int a, int b) {
+    return (a < b) ? a : b;
+}
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    int a;
+
+    fp = fopen(*++argv, "r");
+    while (fscanf(fp, "%d%*c", &a) != EOF) {
+	int i, j, d;
+	int *b = malloc(sizeof(int) * a);
+	fscanf(fp, "%d%*c", &d);
+	b[0] = d;
+	for (i = 1; i < a; i++) {
+	    fscanf(fp, "%d%*c", &d);
+	    b[i] = d + b[i - 1];
+	}
+	for (i = 1; i < a; i++) {
+	    fscanf(fp, "%d%*c", &d);
+	    b[0] += d;
+	    for (j = 1; j < a; j++) {
+		fscanf(fp, "%d%*c", &d);
+		b[j] = d + min(b[j], b[j - 1]);
+	    }
+	}
+	printf("%d\n", b[a - 1]);
+	free(b);
+    }
+    return 0;
+}
