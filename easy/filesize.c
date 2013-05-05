d78c8d1a5b7b0b5d211ab6322d02ef36cf02b524
Nicolas Kaiser <nikai@nikai.net>
2013-05-05 12:18:04 +0200

    add File Size
    C

---
+++ easy/filesize.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    struct stat st;
+    int ret = stat(*++argv, &st);
+    if (ret == 0)
+	printf("%lld\n", (long long)st.st_size);
+    return ret;
+}
