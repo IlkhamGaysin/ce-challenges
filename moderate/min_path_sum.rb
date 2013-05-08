c7eacc82e32ad49c1ac0d55f56aa00d6b236d424
Nicolas Kaiser <nikai@nikai.net>
2013-05-08 22:21:56 +0200

    avoid unnecessary recursions

--- moderate/min_path_sum.rb
+++ moderate/min_path_sum.rb
@@ -1,13 +1,15 @@
 def decode(s)
-  if s.length < 2
-    return 1
-  # should never start with zero: let's ignore if it does
-  elsif s[0].to_i > 2 || s[0] == "0"
-    return decode(s[1..-1])
-  elsif s[1] == "0" || (s[0] == "2" && s[1].to_i > 6)
-    return s.length == 2 ? 1 : decode(s[2..-1])
+  loop do
+    return 1 if s.length < 2
+    # should never start with zero: let's ignore if it does
+    if s[0].to_i > 2 || s[0] == "0"
+      s = s[1..-1]
+    elsif s[1] == "0" || (s[0] == "2" && s[1].to_i > 6)
+      s = s[2..-1]
+    else
+      return s.length == 2 ? 2 : decode(s[1..-1]) + decode(s[2..-1])
+    end
   end
-  s.length == 2 ? 2 : decode(s[1..-1]) + decode(s[2..-1])
 end
 
 File.open(ARGV[0]).each_line do |line|
