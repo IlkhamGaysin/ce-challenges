8565039e19f5044a8a5de49ee6a661450b4271f1
Nicolas Kaiser <nikai@nikai.net>
2014-07-30 19:08:19 +0200

    add Data Recovery
    Go

---
+++ easy/data_recovery.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	reader := bufio.NewReader(data)
+	for {
+		s, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		t := strings.Split(s, ";")
+		words, sequ := strings.Fields(t[0]), strings.Fields(t[1])
+		reco := make([]string, len(words))
+		for ix, i := range sequ {
+			k, err := strconv.Atoi(i)
+			if err != nil {
+				log.Fatal(err)
+			}
+			reco[k-1] = words[ix]
+		}
+		for ix := 0; ix < len(reco); ix++ {
+			if reco[ix] == "" {
+				reco[ix] = words[len(words)-1]
+				break
+			}
+		}
+		fmt.Println(strings.Join(reco, " "))
+	}
+}
