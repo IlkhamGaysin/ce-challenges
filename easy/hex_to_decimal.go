524ca1428d33bad283c4574882d0ec2321fce509
Nicolas Kaiser <nikai@nikai.net>
2014-08-26 15:50:03 +0200

    more Hex to Decimal, Set Intersection
    Go

---
+++ easy/hex_to_decimal.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	reader := bufio.NewReader(data)
+	for {
+		s, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		var d int
+		fmt.Sscanf(string(s), "%x", &d)
+		fmt.Println(d)
+	}
+}
---
+++ easy/intersection.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	reader := bufio.NewReader(data)
+	for {
+		s, _, err := reader.ReadLine()
+		if err != nil {
+			break
+		}
+		t := strings.Split(string(s), ";")
+		u, v := strings.Split(t[0], ","), strings.Split(t[1], ",")
+		x, y := []int{}, []int{}
+		for _, i := range u {
+			var a int
+			fmt.Sscanf(i, "%d", &a)
+			x = append(x, a)
+		}
+		for _, i := range v {
+			var a int
+			fmt.Sscanf(i, "%d", &a)
+			y = append(y, a)
+		}
+		r := []string{}
+		for i, j := 0, 0; i < len(u) && j < len(v); {
+			if x[i] == y[j] {
+				r = append(r, fmt.Sprint(x[i]))
+				i++
+				j++
+			} else if x[i] > y[j] {
+				j++
+			} else {
+				i++
+			}
+		}
+		fmt.Println(strings.Join(r, ","))
+	}
+}
