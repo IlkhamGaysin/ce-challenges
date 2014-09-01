dea6a405f248d20175390733e96b042aad086325
Nicolas Kaiser <nikai@nikai.net>
2014-09-01 10:26:01 +0200

    more Penultimate Word, Sum of Digits
    Bash

---
+++ easy/penultimate_word.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+while read line; do
+    a=( $line )
+    if [ ${#a[*]} -gt 1 ]; then
+        echo ${a[$((${#a[*]}-2))]}
+    else
+        echo
+    fi
+done < $1
---
+++ easy/sum_digits.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+while read line; do
+    res=
+    for ((i=0; i<${#line}; i++)); do
+        res=$(($res+${line:$i:1}))
+    done
+    echo $res
+done < $1
