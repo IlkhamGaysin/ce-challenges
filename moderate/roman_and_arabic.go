7ef6a62d6e430050c09f38a0415a1c232b680a28
Nicolas Kaiser <nikai@nikai.net>
2014-09-03 10:21:47 +0200

    more Roman and Arabic, Valid parentheses
    Go

---
+++ moderate/roman_and_arabic.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	scanner.Split(bufio.ScanBytes)
+	var num, ar, rr int
+	for scanner.Scan() {
+		c := scanner.Text()[0]
+		if c == '\n' {
+			fmt.Printf("%d\n", num+ar*rr)
+			num, ar, rr = 0, 0, 0
+		} else {
+			var r int
+			a := int(c - '0')
+			switch scanner.Scan(); scanner.Text()[0] {
+			case 'M':
+				r = 1000
+			case 'D':
+				r = 500
+			case 'C':
+				r = 100
+			case 'L':
+				r = 50
+			case 'X':
+				r = 10
+			case 'V':
+				r = 5
+			case 'I':
+				r = 1
+			}
+			if r > rr {
+				num -= ar * rr
+			} else {
+				num += ar * rr
+			}
+			ar, rr = a, r
+		}
+	}
+}
---
+++ moderate/valid_parentheses.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	scanner.Split(bufio.ScanBytes)
+	valid := true
+	stack := []byte{}
+	for scanner.Scan() {
+		c := scanner.Text()[0]
+		if !valid {
+			if c == '\n' {
+				valid = true
+			}
+			continue
+		}
+		switch c {
+		case '\n':
+			if len(stack) > 0 {
+				fmt.Println("False")
+				valid = true
+			} else {
+				fmt.Println("True")
+			}
+		case ')':
+			if len(stack) > 0 && stack[0] == '(' {
+				stack = stack[1:len(stack)]
+			} else {
+				valid = false
+			}
+		case ']':
+			if len(stack) > 0 && stack[0] == '[' {
+				stack = stack[1:len(stack)]
+			} else {
+				valid = false
+			}
+		case '}':
+			if len(stack) > 0 && stack[0] == '{' {
+				stack = stack[1:len(stack)]
+			} else {
+				valid = false
+			}
+		default:
+			stack = append([]byte{c}, stack...)
+		}
+		if !valid {
+			fmt.Println("False")
+			stack = []byte{}
+		}
+	}
+}
