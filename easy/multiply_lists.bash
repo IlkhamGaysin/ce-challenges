c9fb2b7f5910338b676cdcd068f17a254244fb99
Nicolas Kaiser <nikai@nikai.net>
2014-09-14 19:51:25 +0200

    more Multiply Lists
    Bash, C, Lua, Ruby

---
+++ easy/multiply_lists.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+while read line; do
+    a=( $line )
+    for ((i=0; i<${#a[*]}/2; i++)); do
+        if [ $i -gt 0 ]; then
+            printf " "
+        fi
+        printf $((${a[i]}*${a[${#a[*]}/2+i+1]}))
+    done
+    echo
+done < $1
---
+++ easy/multiply_lists.rb
@@ -0,0 +1,8 @@
+File.open(ARGV[0]).each_line do |line|
+  s = line.gsub!( /[^\d] /, "").split(" ").map {|s| s.to_i }
+  0.upto(s.length/2-1) { |i|
+    print " " if i > 0
+    print s[i] * s[s.length/2 + i]
+  }
+  puts
+end
