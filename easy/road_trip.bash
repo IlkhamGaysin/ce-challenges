ecf5592c5989dc41a08991e1cce3baaac7ff0f4e
Nicolas Kaiser <nikai@nikai.net>
2014-09-17 15:34:46 +0200

    more Road Trip
    Bash, C

---
+++ easy/road_trip.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+tr -cd "[:digit:] \n" <$1 | while read line; do
+    a=( `echo $line | tr ' ' '\n' | sort -n -S 1M` )
+    printf ${a[0]}
+    for ((c=1; c<${#a[*]}; c++)); do
+        printf ",%d" $((${a[$c]}-${a[$c-1]}))
+    done
+    echo
+done
---
+++ easy/road_trip.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+static int cmpint(const void *p1, const void *p2)
+{
+	return *(int*)p1 - *(int*)p2;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int a;
+	char c;
+
+	fp = fopen(*++argv, "r");
+	do {
+		c = getc(fp);
+	} while (c != ',' && c != EOF);
+	while (fscanf(fp, "%d", &a) != EOF) {
+		int b[600]= { a }, i, n = 1;
+		while (getc(fp) == ';' && getc(fp) == ' ') {
+			do {
+				c = getc(fp);
+			} while (c != ',' && c != EOF);
+			if (fscanf(fp, "%d", &b[n++]) == EOF)
+				goto fail_eof;
+		}
+		qsort(&b, n, sizeof(int), cmpint);
+		for (i = n-1; i > 0; i--) {
+			b[i] -= b[i-1];
+		}
+		printf("%d", b[0]);
+		for (i = 1; i < n; i++) {
+			printf(",%d", b[i]);
+		}
+		printf("\n");
+		do {
+			c = getc(fp);
+		} while (c != ',' && c != EOF);
+	}
+	return 0;
+fail_eof:
+	printf("unexpected end of input\n");
+	return EXIT_FAILURE;
+}
