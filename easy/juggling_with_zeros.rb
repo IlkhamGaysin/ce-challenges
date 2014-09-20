72a6e65f4738a30dd131ba8182d3dd6ab7b8eeb7
Nicolas Kaiser <nikai@nikai.net>
2014-09-20 09:47:43 +0200

    more Juggling With Zeros
    Lua, Ruby

---
+++ easy/juggling_with_zeros.rb
@@ -0,0 +1,8 @@
+File.open(ARGV[0]).each_line do |line|
+  r, s = 0, line.chomp.split.map(&:length)
+  (s.length/2).times do |i|
+    r *= 2**s[i*2+1]
+    r += 2**s[i*2+1]-1 if s[i*2] == 2
+  end
+  puts r
+end
