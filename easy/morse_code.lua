456672a66a8226f5977be34ec1e8b193da089c48
Nicolas Kaiser <nikai@nikai.net>
2014-09-22 18:13:08 +0200

    more Morse Code
    Lua, Ruby

---
+++ easy/morse_code.lua
@@ -0,0 +1,23 @@
+morse = "ETIANMSURWDKGOHVF L PJBXCYZQ  54 3   2       16       7   8 90"
+for line in io.lines(arg[1]) do
+  local m = 1
+  for i in line:gmatch(".") do
+    if i == '.' then
+      m = m * 2
+    elseif i == '-' then
+      m = m * 2 + 1
+    elseif m == 1 then
+      io.write(" ")
+    else
+      if m < 64 then
+        io.write(morse:sub(m-1, m-1))
+      end
+      m = 1
+    end
+  end
+  if m < 64 then
+    print(morse:sub(m-1, m-1))
+  else
+    print()
+  end
+end
