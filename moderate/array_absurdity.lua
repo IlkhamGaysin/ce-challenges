938f25ad7fb69d259ffe2185dc74aec049579bf2
Nicolas Kaiser <nikai@nikai.net>
2014-09-29 22:30:43 +0200

    more Array Absurdity
    Lua, Ruby

---
+++ moderate/array_absurdity.lua
@@ -0,0 +1,13 @@
+for line in io.lines(arg[1]) do
+  if #line > 0 then
+    local sep, m = line:find(";"), {}
+    for i in line:sub(sep):gmatch("%d+") do
+      if m[i] then
+        print(i)
+        break
+      else
+        m[i] = true
+      end
+    end
+  end
+end
