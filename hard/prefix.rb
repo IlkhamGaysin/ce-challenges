c3889684879b6680501516dad20e7b8396990b00
Nicolas Kaiser <nikai@nikai.net>
2014-10-05 23:37:57 +0200

    more Prefix expressions
    Lua, Ruby

---
+++ hard/prefix.rb
@@ -0,0 +1,19 @@
+def pol(o, a, b)
+  case o
+  when '*'
+    return a * b
+  when '/'
+    return a / b
+  end
+  a + b
+end
+
+File.open(ARGV[0]).each_line do |line|
+  s = line.chomp.split
+  n = s.length/2
+  res = s[n].to_f
+  (1..n).each do |x|
+    res = pol(s[n-x], res, s[n+x].to_f)
+  end
+  puts (res + 0.001).to_i
+end
