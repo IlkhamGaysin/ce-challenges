ca70132d58cb849ba342b54401d7d2b982c4d582
Nicolas Kaiser <nikai@nikai.net>
2014-10-06 21:25:09 +0200

    fix Scala objects
    Scala

--- easy/fizzbuzz.scala
+++ easy/fizzbuzz.scala
@@ -1,4 +1,4 @@
-object fizzbuzz extends App {
+object Main extends App {
   val source = scala.io.Source.fromFile(args(0))
   val lines = source.getLines.filter(_.length > 0)
 
--- easy/oddnums.scala
+++ easy/oddnums.scala
@@ -1,3 +1,3 @@
-object oddnums extends App {
+object Main extends App {
   for (i <- 1 to (100,2)) println(i)
 }
