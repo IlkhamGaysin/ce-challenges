bf80116af375acd83e73fe30b3e9a63fd554281f
Nicolas Kaiser <nikai@nikai.net>
2014-10-06 20:40:38 +0200

    more Chain Inspection
    Lua, Ruby

---
+++ moderate/chain_inspection.lua
@@ -0,0 +1,20 @@
+for line in io.lines(arg[1]) do
+  n, b, m, been = 0, "BEGIN", {}, {}
+  for i in line:gmatch("[^;]+") do
+    t, u = i:match("([^-]+)-([^-]+)")
+    m[t], n = u, n + 1
+  end
+  for i = 1, n do
+    b = m[b]
+    if not b or been[b] then
+      b = nil
+      break
+    end
+    been[b] = true
+  end
+  if b == "END" then
+    print("GOOD")
+  else
+    print("BAD")
+  end
+end
---
+++ moderate/chain_inspection.rb
@@ -0,0 +1,16 @@
+File.open(ARGV[0]).each_line do |line|
+  b, s, m, been = "BEGIN", line.chomp.split(";"), Hash.new, Hash.new
+  s.each do |x|
+    t = x.split("-")
+    m[t[0]] = t[1]
+  end
+  s.length.times do |x|
+    if not m.has_key?(b) or been.has_key?(m[b])
+      b = nil
+      break
+    end
+    b = m[b]
+    been[b] = true
+  end
+  puts b == "END" ? "GOOD" : "BAD"
+end
