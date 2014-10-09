e13d8cc6dd28fb01107daa091da4516bcbba5950
Nicolas Kaiser <nikai@nikai.net>
2014-10-09 23:44:20 +0200

    more Lowest Common Ancestor
    Lua, Ruby

---
+++ moderate/lca.rb
@@ -0,0 +1,18 @@
+parent = {3 => 8, 8 => 30, 10 => 20, 20 => 8, 29 => 20, 30 => nil, 52 => 30}
+
+File.open(ARGV[0]).each_line do |line|
+  n, found = line.split.map(&:to_i), false
+  while n[0] and n[0] != n[1]
+    i = n[1]
+    while i
+      if n[0] == i then
+        found = true
+        break
+      end
+      i = parent[i]
+    end
+    break if found
+    n[0] = parent[n[0]]
+  end
+  puts n[0]
+end
