0a9c113cb7dffe60d1493d54e2ec9aeb81a193ba
Nicolas Kaiser <nikai@nikai.net>
2014-10-13 10:05:47 +0200

    more Rightmost Char, Roman Numerals
    Bash

---
+++ easy/rightmost.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+while read line; do
+  c=0
+  p=0
+  while : ; do
+    ((p+=$c))
+    c=`expr index "${line:(-1)}" "${line:$p:(-1)}"`
+    if [ $c -eq 0 ]; then
+      break
+    fi
+  done
+  echo $(( $p-1 ))
+done <$1
