08cb7765031ce3b79107a214a757aac76d0f2e28
Nicolas Kaiser <nikai@nikai.net>
2014-10-19 00:21:24 +0200

    more Armstrong Numbers
    Haskell, Python2

---
+++ easy/armstrong.hs
@@ -0,0 +1,18 @@
+import System.Environment (getArgs)
+import Data.Char (digitToInt)
+
+nthp     :: Int -> Int -> Int
+nthp e i = i ^ e
+
+sumofnthp   :: String -> String
+sumofnthp i = show . sum . map (nthp e) $ map digitToInt i
+            where e = floor (logBase 10 (read i::Double)) + 1
+
+armstrong   :: String -> String
+armstrong i | sumofnthp i == i = "True"
+            | otherwise        = "False"
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map armstrong $ lines input
