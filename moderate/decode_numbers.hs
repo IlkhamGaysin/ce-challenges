f75ed10891a5c6fd9dd0d99c43057d958797de41
Nicolas Kaiser <nikai@nikai.net>
2014-10-21 00:26:21 +0200

    more Decode Numbers, Remove Characters
    Haskell

---
+++ moderate/decode_numbers.hs
@@ -0,0 +1,14 @@
+import System.Environment (getArgs)
+
+decode   :: String -> Int
+decode s | length s < 2                                     = 1
+         | head s /= '1' && head s /= '2'                   = decode t
+         | head t == '0' || (head s == '2' && head t > '6') = decode (tail t)
+         | length s == 2                                    = 2
+         | otherwise                                        = decode t + decode (tail t)
+         where t = tail s
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map show . map decode $ lines input
