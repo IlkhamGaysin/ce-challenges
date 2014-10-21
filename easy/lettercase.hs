c974f5780803248c58100c1ed0f0250e11f0a3e9
Nicolas Kaiser <nikai@nikai.net>
2014-10-21 23:51:57 +0200

    more Lettercase Percentage Ratio
    Haskell

---
+++ easy/lettercase.hs
@@ -0,0 +1,14 @@
+import System.Environment (getArgs)
+import Data.Char (isLower, isUpper)
+import Text.Printf (printf)
+
+ratios   :: String -> String
+ratios s = printf "lowercase: %.2f uppercase: %.2f" (100 * l / t) (100 * u / t)
+         where l = fromIntegral (length [x | x <- s, isLower x]) :: Double
+               u = fromIntegral (length [x | x <- s, isUpper x]) :: Double
+               t = l + u
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map ratios $ lines input
