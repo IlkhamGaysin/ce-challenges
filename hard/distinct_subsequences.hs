f97626a1879ab235a8ec7c68ac1f387e46932f47
Nicolas Kaiser <nikai@nikai.net>
2014-10-23 23:21:58 +0200

    more Distinct Subsequences
    Haskell, Python2

---
+++ hard/distinct_subsequences.hs
@@ -0,0 +1,13 @@
+import System.Environment (getArgs)
+import Data.List.Split (splitOn)
+
+subs   :: [String] -> Int
+subs s | null (last s)                  = 1
+       | null (head s)                  = 0
+       | head (head s) == head (last s) = subs [tail (head s), tail (last s)] + subs [tail (head s), last s]
+       | otherwise                      = subs [tail (head s), last s]
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map show . map subs . map (splitOn ",") $ lines input
