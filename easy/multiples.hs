02368177c84cde694148f5ac4858dbd7cdfbe238
Nicolas Kaiser <nikai@nikai.net>
2014-10-24 17:15:17 +0200

    more Multiples of a Number
    Haskell, Python2

---
+++ easy/multiples.hs
@@ -0,0 +1,12 @@
+import System.Environment (getArgs)
+import Data.List.Split (splitOn)
+
+multiples        :: [Integer] -> Integer
+multiples [a, b] | c == 0    = a
+                 | otherwise = a - c + b
+                 where c = a - floor ((fromIntegral a) * 2 ** (- logBase 2 (fromIntegral b))) * round (2 ** (logBase 2 (fromIntegral b)))
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map show . map multiples . map (map read) . map (splitOn ",") $ lines input
