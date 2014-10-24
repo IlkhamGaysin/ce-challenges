0a45245691252c7e22f9fd9dd39b47fc3ad978ba
Nicolas Kaiser <nikai@nikai.net>
2014-10-24 23:57:53 +0200

    add Read More
    Go

---
+++ easy/read_more.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) > 55 {
+			for i := 40; i > 0; i-- {
+				if line[i] == ' ' {
+					line = line[:i]
+					break
+				}
+			}
+			if len(line) > 55 {
+				line = ""
+			}
+			line += "... <Read More>"
+		}
+		fmt.Println(line)
+	}
+}
