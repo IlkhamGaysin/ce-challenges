56b5d0584bfce744e1fe2430f8b64dd66ef7e528
Nicolas Kaiser <nikai@nikai.net>
2014-10-27 23:29:07 +0100

    more Shortest Repetition
    Haskell

---
+++ easy/shortest_repetition.hs
@@ -0,0 +1,11 @@
+import System.Environment (getArgs)
+
+repetition       :: Int -> Int -> String -> String
+repetition x y xs | x == length xs     = show y
+                  | xs!!x /= xs!!(x-y) = repetition (succ x) (succ x) xs
+                  | otherwise          = repetition (succ x) y xs
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map (repetition 1 1) $ lines input
