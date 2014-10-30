d6ffd9d0b955884dfb5d05457712c8b7fa52a1ae
Nicolas Kaiser <nikai@nikai.net>
2014-10-30 18:15:19 +0100

    more Beautiful Strings
    Haskell

---
+++ easy/beautiful_strings.hs
@@ -0,0 +1,19 @@
+import System.Environment (getArgs)
+import Data.Char (chr, isAlpha, toLower)
+import Data.List (sort)
+
+count :: (Eq a) => a -> [a] -> Int
+count x ys = length (filter (== x) ys)
+
+countChars       :: Int -> [Int] -> String -> [Int]
+countChars i is s | i == 27   = is
+                  | otherwise = countChars (i+1) ((count (chr (97+i)) s) : is) s
+
+beauty       :: Int -> Int -> [Int] -> String
+beauty i x xs | i == 0 || head xs == 0 = show x
+              | otherwise              = beauty (i-1) (head xs * i + x) (tail xs)
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map (beauty 26 0 . reverse . sort . countChars 0 []) $ lines [toLower x | x <- input, isAlpha x || x == '\n']
