bb9159311f1875ede2b2b2980cfd53f6fee157e4
Nicolas Kaiser <nikai@nikai.net>
2014-10-31 13:23:42 +0100

    more Trailing String
    Haskell

---
+++ moderate/sameend.hs
@@ -0,0 +1,15 @@
+import System.Environment (getArgs)
+import Data.List (isSuffixOf)
+import Data.List.Split (splitOn)
+
+nonempty   :: [[String]] -> [[String]]
+nonempty xs = [x | x <- xs, length x == 2]
+
+isSuff   :: [String] -> String
+isSuff xs | isSuffixOf (last xs) (head xs) = "1"
+          | otherwise                      = "0"
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map isSuff . nonempty . map (splitOn ",") $ lines input
