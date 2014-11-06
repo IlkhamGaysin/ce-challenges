f3a021f78b4d62909ad061bc8b507558b7549c83
Nicolas Kaiser <nikai@nikai.net>
2014-11-06 17:37:08 +0100

    improve Robot Movements
    C

--- hard/robot_movements.c
+++ hard/robot_movements.c
@@ -1,67 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct node {
-	int		data;
-	struct node	*next;
-};
+int robot(int f, int x, int y) {
+	int ret = 0;
+	if (x == 3 && y == 3)
+		return 1;
+	if (x > 0 && !(f & (1 << (x-1 + 4*y))))
+		ret += robot(f | (1 << (x-1 + 4*y)), x-1, y);
+	if (y > 0 && !(f & (1 << (x + 4*(y-1)))))
+		ret += robot(f | (1 << (x + 4*(y-1))), x, y-1);
+	if (x < 3 && !(f & (1 << (x+1 + 4*y))))
+		ret += robot(f | (1 << (x+1 + 4*y)), x+1, y);
+	if (y < 3 && !(f & (1 << (x + 4*(y+1)))))
+		ret += robot(f | (1 << (x + 4*(y+1))), x, y+1);
+	return ret;
+}
 
-int main()
-{
-	int i, res;
-	struct node *head = NULL;
-
-	for (i = 0; i < 1 << 27; i += 4) {
-		int j, r = 0, c = 0, cur = i;
-		int been[4][4] = { 1, 0 };
-
-		for (j = 0; j < 15; j++) {
-			switch (cur % 4) {
-			case 0:
-				c++;
-				break;
-			case 1:
-				r++;
-				break;
-			case 2:
-				c--;
-				break;
-			case 3:
-				r--;
-			}
-			if (r < 0 || c < 0 || been[r][c] || r > 3 || c > 3)
-				break;
-			if (r == 3 && c == 3) {
-				int low = i % (1 << j * 2);
-				struct node *temp, *curr = head, *oldr = NULL;
-
-				if (head) {
-					while (curr->data < low) {
-						oldr = curr;
-						if (!curr->next)
-							break;
-						curr = curr->next;
-					}
-				if (curr->data == low)
-					goto out;
-				}
-				temp = malloc(sizeof(struct node));
-				temp->data = low;
-				if (oldr) {
-					temp->next = oldr->next;
-					oldr->next = temp;
-				} else {
-					temp->next = head;
-					head = temp;
-				}
-				res++;
-out:
-				break;
-			}
-			been[r][c] = 1;
-			cur /= 4;
-		}
-	}
-	printf("%d\n", 2 * res);
+int main() {
+	printf("%d\n", robot(1, 0, 0));
 	return 0;
 }
