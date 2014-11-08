1d5dde086aa7a91cfac5b07062ee31817c4a6bc6
Nicolas Kaiser <nikai@nikai.net>
2014-11-08 11:45:17 +0100

    add Filename Pattern
    Go

---
+++ moderate/filename.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		f := strings.Fields(scanner.Text())
+		p, r := regexp.QuoteMeta(f[0]), []string{}
+		p = strings.Replace(p, "\\?", ".", -1)
+		p = strings.Replace(p, "\\*", ".*", -1)
+		p = strings.Replace(p, "\\[", "[", -1)
+		p = strings.Replace(p, "\\]", "]", -1)
+		p = "^" + p + "$"
+		pattern := regexp.MustCompile(p)
+		for i := 1; i < len(f); i++ {
+			if pattern.MatchString(f[i]) {
+				r = append(r, f[i])
+			}
+		}
+		if len(r) > 0 {
+			fmt.Println(strings.Join(r, " "))
+		} else {
+			fmt.Println("-")
+		}
+	}
+}
