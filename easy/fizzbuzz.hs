42d79c66cc8649c447291952c1bb4fcc4c66aa0b
Nicolas Kaiser <nikai@nikai.net>
2014-11-08 20:38:08 +0100

    more Fizz Buzz
    Haskell, Python2

---
+++ easy/fizzbuzz.hs
@@ -0,0 +1,15 @@
+import System.Environment (getArgs)
+
+fizz        :: Int -> [String] -> [Int] -> [String]
+fizz i xs ys | i > last ys                  = xs
+             | mod i f == 0 && mod i b == 0 = fizz (succ i) (xs ++ ["FB"]) ys
+             | mod i f == 0                 = fizz (succ i) (xs ++ ["F"]) ys
+             | mod i b == 0                 = fizz (succ i) (xs ++ ["B"]) ys
+             | otherwise                    = fizz (succ i) (xs ++ [show i]) ys
+             where f = head ys
+                   b = ys!!1
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map (unwords . fizz 1 [] . map read . words) $ lines input
