779f4958933890ae5c61caa5cf2db83f81077b0a
Nicolas Kaiser <nikai@nikai.net>
2014-11-10 17:59:12 +0100

    more Compressed Sequence
    Haskell

---
+++ easy/compressed_sequence.hs
@@ -0,0 +1,11 @@
+import System.Environment (getArgs)
+
+compress      :: [Int] -> [Int] -> [Int]
+compress xs ys | null ys                       = xs
+               | null xs || last xs /= head ys = compress (xs ++ [1, head ys]) (tail ys)
+               | otherwise                     = compress ((init $ init xs) ++ [succ . last $ init xs] ++ [last xs]) (tail ys)
+
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map (unwords . map show . compress [] . map read . words) $ lines input
