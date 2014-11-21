00260eca55f07d1e78e5edb3b7f2c64ff9c9fb8a
Nicolas Kaiser <nikai@nikai.net>
2014-11-21 15:15:29 +0100

    improve Prime Numbers
    Haskell

--- moderate/prime_less.hs
+++ moderate/prime_less.hs
@@ -1,17 +1,18 @@
 import System.Environment (getArgs)
 import Data.List (intercalate)
 
-factors  :: Int -> [Int]
-factors n = [x | x <- [1..n], mod n x == 0]
-
-prime  :: Int -> Bool
-prime x = factors x == [1, x]
+prime     :: [Int] -> Int -> Bool
+prime _ 2  = True
+prime xs n | x*x > n      = True
+           | mod n x == 0 = False
+           | otherwise    = prime (tail xs) n
+           where x = head xs
 
 primesTo       :: [Int] -> Int -> Int -> [Int]
-primesTo xs i n | i >= n        = xs
-                | i == 2        = primesTo [2] 3 n
-                | not (prime i) = primesTo xs (i + 2) n
-                | otherwise     = primesTo (xs ++ [i]) (i + 2) n
+primesTo xs i n | i >= n           = xs
+                | i == 2           = primesTo [2] 3 n
+                | not (prime xs i) = primesTo xs (i + 2) n
+                | otherwise        = primesTo (xs ++ [i]) (i + 2) n
 
 main :: IO ()
 main = do
