fbcda8aaab05366166115e5d344f25e769a904ae
Nicolas Kaiser <nikai@nikai.net>
2014-11-26 15:11:21 +0100

    more N Mod M
    R

---
+++ easy/nmodm.r
@@ -0,0 +1,7 @@
+nmodm <- function(s) {
+  n <- as.integer(s[1])
+  m <- as.integer(s[2])
+  n - as.integer(n / m) * m
+}
+
+cat(sapply(strsplit(readLines(tail(commandArgs(), n=1)), ","), nmodm), sep="\n")
