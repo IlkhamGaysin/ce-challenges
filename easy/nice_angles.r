699a205e82a66d754cf3bf4d5149e109f0cfb104
Nicolas Kaiser <nikai@nikai.net>
2014-11-27 00:25:20 +0100

    more Nice angles
    R

---
+++ easy/nice_angles.r
@@ -0,0 +1,15 @@
+angle <- function(s) {
+  a <- as.integer(s)
+  r <- paste(toString(a), ".", sep="")
+  s <- (s - a) * 60
+  a <- as.integer(s)
+  if (a < 10) { f <- "0" } else { f <- "" }
+  r <- paste(r, f, toString(a), "'", sep="")
+  s <- (s - a) * 60
+  a <- as.integer(s)
+  if (a < 10) { f <- "0" } else { f <- "" }
+  r <- paste(r, f, toString(a), "\"", sep="")
+  r
+}
+
+cat(sapply(as.numeric(readLines(tail(commandArgs(), n=1))), angle), sep="\n")
