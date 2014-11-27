8813897cf4ea8a3271076e05d722334c93b89069
Nicolas Kaiser <nikai@nikai.net>
2014-11-27 23:36:26 +0100

    more Age distribution
    R

---
+++ easy/age_distribution.r
@@ -0,0 +1,14 @@
+categories <- c("This program is for humans", "Still in Mama's arms", "Preschool Maniac", "Elementary school", "Middle school", "High school", "College", "Working for the man", "The Golden Years")
+ages <- c(0, 3, 5, 12, 15, 19, 23, 66, 101)
+age <- function(s) {
+  i <- 1
+  while (i <= length(ages) && s >= ages[i]) {
+    i <- i + 1
+  }
+  if (i > length(ages)) {
+    i <- 1
+  }
+  categories[i]
+}
+
+cat(sapply(as.integer(readLines(tail(commandArgs(), n=1))), age), sep="\n")
