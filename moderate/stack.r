a964db10303e05cdf72f0052727be2ba68fba3e7
Nicolas Kaiser <nikai@nikai.net>
2014-12-01 22:38:17 +0100

    more Stack Implementation
    R

---
+++ moderate/stack.r
@@ -0,0 +1,29 @@
+push <- function(s, x) {
+  s <<- append(s, x)
+}
+
+pop <- function(s) {
+  x <- s[[length(s)]]
+  length(s) <<- length(s) - 1
+  x
+}
+
+s <- c()
+stack <- function(ss) {
+  s <<- c()
+  for (i in 1:length(ss)) {
+    s <<- push(s, ss[[i]])
+  }
+  h <- TRUE
+  r <- c()
+  for (i in 1:length(ss)) {
+    x <- pop(s)
+    if (h) {
+      r <- append(r, x)
+    }
+    h <- !h
+  }
+  paste(r, collapse=" ")
+}
+
+cat(sapply(strsplit(readLines(tail(commandArgs(), n=1)), " "), stack), sep="\n")
