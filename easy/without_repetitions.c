9f296207feeee7bf63691254655ec6fb2cc50c12
Nicolas Kaiser <nikai@nikai.net>
2014-12-05 11:57:23 +0100

    add Without Repetitions
    C, Haskell

---
+++ easy/without_repetitions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char c, p = '\0';
+
+	fp = fopen(*++argv, "r");
+	while ((c = getc(fp)) != EOF) {
+		if (c != p)
+			putchar(c);
+		p = c;
+	}
+	return 0;
+}
+
