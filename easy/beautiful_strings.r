4cd8652e4ad26391d8119d09b7dca6a2ffcbb347
Nicolas Kaiser <nikai@nikai.net>
2014-12-05 15:16:09 +0100

    more Beautiful Strings
    R

---
+++ easy/beautiful_strings.r
@@ -0,0 +1,19 @@
+beautiful <- function(s) {
+  xs <- strsplit(s, "")[[1]]
+  ct <- rep.int(0, 26)
+  for (i in 1:length(xs)) {
+    if (xs[i] >= 'a' && xs[i] <= 'z') {
+      ct[utf8ToInt(xs[i]) - 96] <- ct[utf8ToInt(xs[i]) - 96] + 1
+    }
+  }
+  ct <- sort.int(ct)
+  i <- 26
+  b <- 0
+  while (i > 0 && ct[i] > 0) {
+    b <- b + i * ct[i]
+    i <- i - 1
+  }
+  b
+}
+
+cat(sapply(tolower(readLines(tail(commandArgs(), n=1))), beautiful), sep="\n")
