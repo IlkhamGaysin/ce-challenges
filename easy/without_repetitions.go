af8917be6c3fe5174ecf1667a0198e70773b4bf5
Nicolas Kaiser <nikai@nikai.net>
2014-12-06 22:49:52 +0100

    more Without Repetitions
    Go, Lua

---
+++ easy/without_repetitions.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(data)
+	scanner.Split(bufio.ScanBytes)
+	var c string
+	for scanner.Scan() {
+		if c != scanner.Text() {
+			fmt.Print(scanner.Text())
+		}
+		c = scanner.Text()
+	}
+}
