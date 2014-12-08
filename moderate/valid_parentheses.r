f0cc62730049f6e2becfa8388ae2e03b2061fc54
Nicolas Kaiser <nikai@nikai.net>
2014-12-08 13:31:54 +0100

    more Valid parentheses
    R

---
+++ moderate/valid_parentheses.r
@@ -0,0 +1,24 @@
+paren <- function(xs, ys) {
+  if (length(ys) == 0 || is.na(ys[1])) {
+    return(length(xs) == 0 || is.na(xs[1]))
+  }
+  if (length(xs) > 0) {
+    if ((ys[1] == ")" && xs[1] == "(") || (ys[1] == "]" && xs[1] == "[") || (ys[1] == "}" && xs[1] == "{")) {
+      return(paren(xs[2:length(xs)], ys[2:length(ys)]))
+    }
+  }
+  if (ys[1] %in% ")]}") {
+    return(FALSE)
+  }
+  paren(c(ys[1], xs, recursive=TRUE), ys[2:length(ys)])
+}
+
+parens <- function(s) {
+  if (paren(list(), strsplit(s, "")[[1]])) {
+    "True"
+  } else {
+    "False"
+  }
+}
+
+cat(sapply(readLines(tail(commandArgs(), n=1)), parens), sep="\n")
