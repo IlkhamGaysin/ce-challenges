fb9126696578f5398cd41142ccf58d4b9c8b46b8
Nicolas Kaiser <nikai@nikai.net>
2014-12-09 22:24:19 +0100

    more Bit Positions
    R

---
+++ easy/position.r
@@ -0,0 +1,13 @@
+position <- function(s) {
+  xs <- strsplit(s, ",")[[1]]
+  x <- as.integer(xs[1])
+  p1 <- 2^(as.integer(xs[2])-1)
+  p2 <- 2^(as.integer(xs[3])-1)
+  if ((x %% (p1+p1) >= p1) == (x %% (p2+p2) >= p2)) {
+    "true"
+  } else {
+    "false"
+  }
+}
+
+cat(sapply(readLines(tail(commandArgs(), n=1)), position), sep="\n")
