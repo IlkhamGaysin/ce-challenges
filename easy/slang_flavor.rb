105484ce9ddeea10124afeb1160071488d2675e6
Nicolas Kaiser <nikai@nikai.net>
2014-12-21 23:43:18 +0100

    more Slang Flavor
    Ruby

---
+++ easy/slang_flavor.rb
@@ -0,0 +1,24 @@
+phrases = [", yeah!",
+	   ", this is crazy, I tell ya.",
+	   ", can U believe this?",
+	   ", eh?",
+	   ", aw yea.",
+	   ", yo.",
+	   "? No way!",
+	   ". Awesome!"]
+i, l = 0, false
+File.open(ARGV[0]).each_line do |line|
+  s = line.split("").each {|x|
+    if x == "." or x == "!" or x == "?"
+      if l
+        print phrases[i]
+        i, l = (i+1) % 8, false
+      else
+        print x
+        l = true
+      end
+    else
+      print x
+    end
+  }
+end
