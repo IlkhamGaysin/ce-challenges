0d283aec13831b8ac409c33b04d05dc3d0a91483
Nicolas Kaiser <nikai@nikai.net>
2015-01-04 11:49:49 +0100

    more JSON menu IDs
    C

---
+++ easy/json_menu_ids.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	char c, *id = "\"id\":";
+	int i = 0, s = -1, n;
+
+	fp = fopen(*++argv, "r");
+	while ((c = getc(fp)) != EOF) {
+		if (s < 0 && c == '{') {
+			s = 0;
+		} else if (c == '\n') {
+			if (s >= 0) {
+				printf("%d\n", s);
+				s = -1;
+			}
+			i = 0;
+		} else if (i == 5) {
+			fscanf(fp, " %d", &n);
+			if (getc(fp) == ',')
+				s += n;
+			i = 0;
+		} else if (c == id[i]) {
+			i++;
+		} else {
+			i = 0;
+		}
+	}
+	return 0;
+}
