dfc15bb62676cf3db0a4441e1359ecd2b42ae868
Nicolas Kaiser <nikai@nikai.net>
2015-01-07 23:55:37 +0100

    more Racing Chars
    Ruby

---
+++ easy/racing_chars.rb
@@ -0,0 +1,31 @@
+p, q = nil, nil
+File.open(ARGV[0]).each_line do |line|
+  if p == nil
+    p = line.index('C')
+    if p == nil
+      p = line.index('_')
+    end
+    line[p] = "|"
+  else
+    loop do
+      q = line.index('C')
+      break if q == nil or (p-q).abs < 2
+    end
+    if q == nil
+      loop do
+        q = line.index('_')
+        break if (p-q).abs < 2
+      end
+    end
+    if q < p
+      sym = "/"
+    elsif q == p
+      sym = "|"
+    else
+      sym = "\\"
+    end
+    line[q] = sym
+    p = q
+  end
+  puts line
+end
