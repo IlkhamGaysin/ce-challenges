8b26f0ae35981fc8f545cf14ed9bfef484bcd1fe
Nicolas Kaiser <nikai@nikai.net>
2015-01-20 17:01:40 +0100

    improve File Size
    and add constraints to description

--- easy/filesize.c
+++ easy/filesize.c
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    struct stat st;
-    int ret = stat(*++argv, &st);
-    if (ret == 0)
-	printf("%lld\n", (long long)st.st_size);
-    return ret;
+	struct stat st;
+	int ret = stat(*++argv, &st);
+	if (ret == 0)
+		printf("%lld\n", (long long)st.st_size);
+	return ret;
 }
