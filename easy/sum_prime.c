6c3d1dac792922ada019607aa7eb3d336a0167bb
Nicolas Kaiser <nikai@nikai.net>
2015-02-10 16:43:32 +0100

    improve Sum of Primes
    and add constraints to description

--- easy/sum_prime.c
+++ easy/sum_prime.c
@@ -12,7 +12,7 @@ bool prime(int a, int *p) {
 
 int main(int argc, char *argv[])
 {
-	int primes[1000] = {2, 3};
+	int primes[1000] = { 2, 3 };
 	int i, c = 5, s = 5;
 	for (i = 2; i < 1000; i++) {
 		while (!prime(c, primes))
