4b2e06d03e95a24154034b6cd4e90e678bce3ad3
Nicolas Kaiser <nikai@nikai.net>
2015-02-13 21:44:29 +0100

    add Details
    Go

---
+++ easy/details.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), ",")
+		m := len(s[0])
+		for _, i := range s {
+			if strings.Contains(i, "XY") {
+				m = 0
+			} else {
+				n := strings.Count(i, ".")
+				if n < m {
+					m = n
+				}
+			}
+		}
+		fmt.Println(m)
+	}
+}
