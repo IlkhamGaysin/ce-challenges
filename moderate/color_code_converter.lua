6e539dc925763befd308221cd11f56f10116cea9
Nicolas Kaiser <nikai@nikai.net>
2015-02-13 14:28:16 +0100

    improve various
    Lua, and restore renamed Python 2 file

--- moderate/color_code_converter.lua
+++ moderate/color_code_converter.lua
@@ -5,10 +5,10 @@ end
 for line in io.lines(arg[1]) do
   local r, g, b = 0, 0, 0
   if line:sub(1, 1) == "#" then
-    _, _, r, g, b = line:find("(%x%x)(%x%x)(%x%x)")
+    r, g, b = line:match("(%x%x)(%x%x)(%x%x)")
     r, g, b = tonumber("0x" .. r), tonumber("0x" .. g), tonumber("0x" .. b)
   elseif line:sub(1, 3) == "HSL" then
-    _, _, h, s, l = line:find("HSL%((%d+),(%d+),(%d+)%)")
+    h, s, l = line:match("HSL%((%d+),(%d+),(%d+)%)")
     s, l = s/100, l/100
     local c, h2 = (1-math.abs(2*l-1))*s, h/60
     local x, m = c*(1-math.abs(h2%2 - 1)), l-c/2
@@ -26,7 +26,7 @@ for line in io.lines(arg[1]) do
       r, g, b = rint(c+m), rint(m), rint(x+m)
     end
   elseif line:sub(1, 3) == "HSV" then
-    _, _, h, s, v = line:find("HSV%((%d+),(%d+),(%d+)%)")
+    h, s, v = line:match("HSV%((%d+),(%d+),(%d+)%)")
     s, v = s/100, v/100
     local c, h2 = v*s, h/60
     local x, m = c*(1-math.abs(h2%2 - 1)), v-c
@@ -44,7 +44,7 @@ for line in io.lines(arg[1]) do
       r, g, b = rint(c+m), rint(m), rint(x+m)
     end
   elseif line:sub(1, 1) == "(" then
-    _, _, c, m, y, k = line:find("%(([%d.]+),([%d.]+),([%d.]+),([%d.]+)%)")
+    c, m, y, k = line:match("%(([%d.]+),([%d.]+),([%d.]+),([%d.]+)%)")
     r, g, b = rint((1-c)*(1-k)), rint((1-m)*(1-k)), rint((1-y)*(1-k))
   end
   print("RGB(" .. r .. "," .. g .. "," .. b .. ")")
