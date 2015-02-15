8a5c93f43103a20f2c27766ae32346c2f23730e9
Nicolas Kaiser <nikai@nikai.net>
2015-02-15 17:17:56 +0100

    improve Robot Movements
    and add constraints to description

--- hard/robot_movements.c
+++ hard/robot_movements.c
@@ -4,14 +4,14 @@ int robot(int f, int x, int y) {
 	int ret = 0;
 	if (x == 3 && y == 3)
 		return 1;
-	if (x > 0 && !(f & (1 << (x-1 + 4*y))))
-		ret += robot(f | (1 << (x-1 + 4*y)), x-1, y);
-	if (y > 0 && !(f & (1 << (x + 4*(y-1)))))
-		ret += robot(f | (1 << (x + 4*(y-1))), x, y-1);
-	if (x < 3 && !(f & (1 << (x+1 + 4*y))))
-		ret += robot(f | (1 << (x+1 + 4*y)), x+1, y);
-	if (y < 3 && !(f & (1 << (x + 4*(y+1)))))
-		ret += robot(f | (1 << (x + 4*(y+1))), x, y+1);
+	if (x > 0 && !(f & (1 << (x - 1 + 4 * y))))
+		ret += robot(f | (1 << (x - 1 + 4 * y)), x - 1, y);
+	if (y > 0 && !(f & (1 << (x + 4 * (y - 1)))))
+		ret += robot(f | (1 << (x + 4 * (y - 1))), x, y - 1);
+	if (x < 3 && !(f & (1 << (x + 1 + 4 * y))))
+		ret += robot(f | (1 << (x + 1 + 4 * y)), x + 1, y);
+	if (y < 3 && !(f & (1 << (x + 4 * (y + 1)))))
+		ret += robot(f | (1 << (x + 4 * (y + 1))), x, y + 1);
 	return ret;
 }
 
