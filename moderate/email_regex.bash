c44b6fd20bfc9d1a543dd74c5e613cca694c7b52
Nicolas Kaiser <nikai@nikai.net>
2015-02-19 21:51:07 +0100

    improve Email Validation
    and add constraints to description

--- moderate/email_regex.bash
+++ moderate/email_regex.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 email_regex="^((\"[0-9A-Za-z@.+-=]+\")|([0-9A-Za-z.+-=]+))@\w*(\w+\.)+\w{2,4}$"
 
-while read -r line; do
+while read -r line || [ -n "$line" ]; do
     if [[ "$line" =~ $email_regex ]]; then
         echo true
     else
         echo false
     fi
-done < $1
+done <$1
--- moderate/email_regex.r
+++ moderate/email_regex.r
@@ -1,7 +1,6 @@
 email <- "^((\"[0-9A-Za-z@.+-=]+\")|([0-9A-Za-z.+-=]+))@[0-9A-Za-z_]*([0-9A-Za-z_]+[.])+[0-9A-Za-z_]{2,4}$"
-checkemail <- function(b) {
+
+cat(sapply(grepl(email, readLines(tail(commandArgs(), n=1))), function(b) {
   if (b) return("true")
   "false"
-}
-
-cat(sapply(grepl(email, readLines(tail(commandArgs(), n=1))), checkemail), sep="\n")
+}), sep="\n")
