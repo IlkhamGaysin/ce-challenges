4a4c14909d33722218d190e30d26ad8f898e4182
Nicolas Kaiser <nikai@nikai.net>
2015-02-21 21:52:41 +0100

    improve Decode Numbers
    and add constraints to description

--- moderate/decode_numbers.c
+++ moderate/decode_numbers.c
@@ -5,11 +5,11 @@ int decode(char *sb) {
 	if (sb[0] == '\0' || sb[1] == '\0') {
 		return 1;
 	} else if (sb[0] != '1' && sb[0] != '2') {
-		return decode(sb+1);
+		return decode(sb + 1);
 	} else if (sb[1] == '0' || (sb[0] == '2' && sb[1] > '6')) {
-		return decode(sb+2);
+		return decode(sb + 2);
 	}
-	return decode(sb+2) + decode(sb+1);
+	return decode(sb + 2) + decode(sb + 1);
 }
 
 int main(int argc, char *argv[])
@@ -20,14 +20,14 @@ int main(int argc, char *argv[])
 	char *sb = malloc(sbs);
 
 	fp = fopen(*++argv, "r");
-	while ((c = getc(fp)) != EOF) {
-		if (c == '\n') {
+	while ((c = getc(fp)) != EOF || i > 0) {
+		if (c == '\n' || c == EOF) {
 			sb[i] = '\0';
 			printf("%d\n", decode(sb));
 			i = 0;
 		} else {
-			if (i == sbs-1) {
-				sbs += sbs/2;
+			if (i == sbs - 1) {
+				sbs += sbs / 2;
 				sb = realloc(sb, sbs);
 			}
 			sb[i++] = c;
