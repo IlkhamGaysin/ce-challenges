18e88cc1a9c1e46bbfed72b92fd7d2216efa387c
Nicolas Kaiser <nikai@nikai.net>
2015-02-25 12:28:55 +0100

    improve Double Squares
    and add constraints to description

--- moderate/double_squares.go
+++ moderate/double_squares.go
@@ -23,20 +23,14 @@ func main() {
 		log.Fatal(err)
 	}
 	defer data.Close()
-	reader := bufio.NewReader(data)
-	s, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanner := bufio.NewScanner(data)
+	scanner.Scan()
 	var n int
-	fmt.Sscan(s, &n)
+	fmt.Sscan(scanner.Text(), &n)
 	for i := 0; i < n; i++ {
-		s, err := reader.ReadString('\n')
-		if err != nil {
-			break
-		}
+		scanner.Scan()
 		var x int
-		fmt.Sscan(s, &x)
+		fmt.Sscan(scanner.Text(), &x)
 		num, l, bot, top := 0, make(map[int]bool), sqrt(x/2), sqrt(x)+1
 		for j := bot; j < top; j++ {
 			t := x - j*j
