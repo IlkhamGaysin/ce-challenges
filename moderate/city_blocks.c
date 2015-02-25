e50fd8ee066685a150e668e5afadb0e5ecfbc2ca
Nicolas Kaiser <nikai@nikai.net>
2015-02-25 18:16:06 +0100

    improve City Blocks Flyover
    and add constraints to description
    
    Note that these constraints do not match any of the given input samples
    which all include streets and avenues at distance zero.

--- moderate/city_blocks.c
+++ moderate/city_blocks.c
@@ -22,13 +22,13 @@ int main(int argc, char *argv[])
 		} else {
 			int its = 0, stl, avl;
 			if (st[0])
-				for (i = nst-1; i >= 0; i--)
+				for (i = nst - 1; i >= 0; i--)
 					st[i] -= st[0];
 			if (av[0])
-				for (i = nav-1; i >= 0; i--)
+				for (i = nav - 1; i >= 0; i--)
 					av[i] -= av[0];
-			stl = st[nst-1];
-			avl = av[nav-1];
+			stl = st[nst - 1];
+			avl = av[nav - 1];
 			for (i = 0; i < nav; i++)
 				av[i] = av[i] * stl / avl;
 			for (i = 0, j = 0; i < nst && j < nav;) {
