7d3ec8fe8884ed667b50c88be88dd68a24a189d3
Nicolas Kaiser <nikai@nikai.net>
2015-03-09 16:28:16 +0100

    more Max Range Sum
    C

---
+++ easy/max_range_sum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int n, d[99];
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d;", &n) != EOF) {
+		int m = 0, cm = 0, c, i = 0;
+		do {
+			fscanf(fp, "%d", &c);
+			if (i < n) {
+				cm += c;
+				d[i++] = c;
+			} else {
+				cm += c - d[i % n];
+				d[i++ % n] = c;
+			}
+			if (i >= n && cm > m)
+				m = cm;
+		} while (getc(fp) == ' ');
+		printf("%d\n", m);
+	}
+	return 0;
+}
