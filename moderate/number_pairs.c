6fb21459990c15810ed7eb42186c48881a763cfc
Nicolas Kaiser <nikai@nikai.net>
2015-03-29 13:26:12 +0200

    more Number Pairs
    C

---
+++ moderate/number_pairs.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int a, i = 0, ibs = 32;
+	int *ib = malloc(ibs * sizeof(int));
+	char c;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d", &a) != EOF) {
+		if (i == ibs) {
+			ibs += ibs / 2;
+			ib = realloc(ib, ibs * sizeof(int));
+		}
+		ib[i++] = a;
+		if ((c = getc(fp)) == ';') {
+			fscanf(fp, "%d", &a);
+			int n = 0, j, k;
+			for (j = 0; j < i - 1 && 2 * ib[j] < a; j++)
+				for (k = i - 1; k > j && ib[j] + ib[k] >= a; k--)
+					if (ib[j] + ib[k] == a) {
+						if (n++ > 0)
+							putchar(';');
+						printf("%d,%d", ib[j], ib[k]);
+					}
+			if (n == 0)
+				printf("NULL");
+			putchar('\n');
+			i = 0;
+		}
+	}
+	free(ib);
+	return 0;
+}
