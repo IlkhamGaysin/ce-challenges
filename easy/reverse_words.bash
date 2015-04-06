5267a7e3370fe86ab36ad0eb3782c3f8c8e8994a
Nicolas Kaiser <nikai@nikai.net>
2015-04-06 09:20:24 +0200

    improve Reverse words
    Bash

--- easy/reverse_words.bash
+++ easy/reverse_words.bash
@@ -1,9 +1,9 @@
 while read line || [ -n "$line" ]; do
   if [ -n "$line" ]; then
     a=( $line )
-    l=$((${#a[@]}-1))
-    printf "${a[$l]}"
-    for ((i=$l-1; i>=0; i--)); do
+    i=$((${#a[@]}-1))
+    printf ${a[$i]}
+    for ((i--; i>=0; i--)); do
         printf " ${a[$i]}"
     done
     echo
