3d7db1bbffa6ed21c68bdf552d206c4e4085f1b2
Nicolas Kaiser <nikai@nikai.net>
2015-04-11 21:53:06 +0200

    more Number Pairs
    Bash, Ruby

---
+++ moderate/number_pairs.rb
@@ -0,0 +1,21 @@
+File.open(ARGV[0]).each_line do |line|
+  s = line.split(';')
+  t, u = s[0].split(',').map(&:to_i), s[1].to_i
+  n, i, j = false, 0, t.length - 1
+  while i < j and 2 * t[i] < u do
+    while j > i and t[i] + t[j] >= u do
+      if t[i] + t[j] == u
+        if n
+          print ';'
+        else
+          n = true
+        end
+        printf "%d,%d", t[i], t[j]
+      end
+      j -= 1
+    end
+    i += 1
+  end
+  print "NULL" if not n
+  puts
+end
