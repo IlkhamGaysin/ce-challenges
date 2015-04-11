07c992573fdecfa23d0ce2afd50a3f1895b07b0d
Nicolas Kaiser <nikai@nikai.net>
2015-04-11 17:05:46 +0200

    more Cracking eggs
    Ruby

---
+++ hard/cracking_eggs.rb
@@ -0,0 +1,13 @@
+def floors(e, s)
+  return 1 if s == 1
+  return s if e == 1
+  return 0 if e == 0 or s == 0
+  return floors(s, s) if e > s
+  floors(e - 1, s - 1) + floors(e, s - 1) + 1
+end
+
+File.open(ARGV[0]).each_line do |line|
+  s, n = line.split.map(&:to_i), 0
+  while floors(s[0], n) < s[1] do n += 1 end
+  puts n
+end
