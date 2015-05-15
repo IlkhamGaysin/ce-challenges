05400a57c1f1cb541ab64b4de45def262b132f5a
Nicolas Kaiser <nikai@nikai.net>
2015-05-15 19:08:00 +0200

    add Swap Numbers
    Bash, Go, Haskell

---
+++ easy/swap_numbers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		s := strings.Fields(scanner.Text())
+		for ix, i := range s {
+			s[ix] = string(i[len(i)-1]) + i[1:len(i)-1] + string(i[0])
+		}
+		fmt.Println(strings.Join(s, " "))
+	}
+}
