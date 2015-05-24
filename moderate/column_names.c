28ff6f6af8c01b1c4519e2343352e32a67b48ec1
Nicolas Kaiser <nikai@nikai.net>
2015-05-24 16:12:12 +0200

    more Column Names
    Bash, C

---
+++ moderate/column_names.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int a;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d", &a) != EOF) {
+		int b, c, d = --a % 26;
+		if (a >= 26) {
+			a /= 26;
+			c = --a % 26;
+			if (a >= 26) {
+				a /= 26;
+				b = --a % 26;
+				printf("%c%c%c\n", 'A' + b, 'A' + c, 'A' + d);
+			} else {
+				printf("%c%c\n", 'A' + c, 'A' + d);
+			}
+		} else {
+			printf("%c\n", 'A' + d);
+		}
+	}
+	return 0;
+}
