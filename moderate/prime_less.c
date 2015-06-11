4db3f18bb1a7079f1abd9aac2b100bbcd18f2fdc
Nicolas Kaiser <nikai@nikai.net>
2015-06-11 16:15:42 +0200

    more Prime Numbers
    C

---
+++ moderate/prime_less.c
@@ -0,0 +1,43 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool prime(int a, int *p) {
+	while (*p * *p <= a) {
+		if (a % *p == 0)
+			return false;
+		p++;
+	}
+	return true;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int i, n, last = 3, np = 2, ps = 8;
+	int *primes = malloc(ps * sizeof(int));
+	primes[0] = 2;
+	primes[1] = 3;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d", &n) != EOF) {
+		while (last + 2 < n) {
+			last += 2;
+			if (prime(last, primes)) {
+				if (ps == np) {
+					ps += ps / 2;
+					primes = realloc(primes, ps * sizeof(int));
+				}
+				primes[np++] = last;
+			}
+		}
+		if (n >= 2) {
+			putchar('2');
+			for (i = 1; i < np && primes[i] < n; i++)
+				printf(",%d", primes[i]);
+		}
+		putchar('\n');
+	}
+	free(primes);
+	return 0;
+}
