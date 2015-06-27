ba8f3d011b0c7ed69b97daecb52313d2c56d3718
Nicolas Kaiser <nikai@nikai.net>
2015-06-27 15:36:56 +0200

    more Stepwise word
    C, Lua

---
+++ easy/stepwise_word.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int l = 0, i = 0, j;
+	char c, sb[10], cb[10];
+
+	fp = fopen(*++argv, "r");
+	while ((c = getc(fp)) != EOF || i > 0) {
+		if (c == ' ' || c == '\n' || c == EOF) {
+			if (i > l)
+				for (l = 0; l < i; l++)
+					sb[l] = cb[l];
+			if (c != ' ') {
+				for (i = 0; i < l; i++) {
+					if (i > 0)
+						putchar(' ');
+					for (j = 0; j < i; j++)
+						putchar('*');
+					putchar(sb[i]);
+				}
+				putchar('\n');
+				l = 0;
+			}
+			i = 0;
+		} else {
+			i %= 10;
+			cb[i++] = c;
+		}
+	}
+	return 0;
+}
