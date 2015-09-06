1d4187616a5818d50aaa6a867cb173be88e67e0b
Nicolas Kaiser <nikai@nikai.net>
2015-09-06 12:59:13 +0200

    more Robo and Robitta
    C

---
+++ moderate/robo_and_robitta.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	unsigned a, b, x, y;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%dx%d | %d %d", &a, &b, &x, &y) != EOF) {
+		unsigned r = 0, t;
+		while (b > y) {
+			r += a;
+			t = a;
+			a = b - 1;
+			b = t;
+			t = x;
+			x = a + 1 - y;
+			y = t;
+		}
+		printf("%d\n", r + x);
+	}
+	return 0;
+}
