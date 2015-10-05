5eebd972d3e1f5bcac51dc621025d0ca7948de3a
Nicolas Kaiser <nikai@nikai.net>
2015-10-05 18:53:17 +0200

    add Everything or Nothing
    Go

---
+++ hard/everything_or_nothing.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	read  = 4
+	write = 2
+	grant = 1
+)
+
+func d(s uint8) uint8 {
+	return s - 49
+}
+
+func main() {
+	var uperm [][]int
+	data, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer data.Close()
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		s, valid := strings.Fields(scanner.Text()), true
+		uperm = [][]int{[]int{7, 3, 0}, []int{6, 2, 4}, []int{5, 1, 5},
+			[]int{3, 7, 1}, []int{6, 0, 2}, []int{4, 2, 6}}
+		for _, i := range s {
+			w, f := d(i[5]), d(i[13])
+			switch i[16] {
+			case 'r':
+				if uperm[w][f]&read == 0 {
+					valid = false
+				}
+			case 'w':
+				if uperm[w][f]&write == 0 {
+					valid = false
+				}
+			case 'g':
+				if uperm[w][f]&grant == 0 {
+					valid = false
+				}
+				switch i[23] {
+				case 'r':
+					uperm[d(i[34])][f] |= read
+				case 'w':
+					uperm[d(i[35])][f] |= write
+				case 'g':
+					uperm[d(i[35])][f] |= grant
+				}
+			}
+			if !valid {
+				break
+			}
+		}
+		if valid {
+			fmt.Println("True")
+		} else {
+			fmt.Println("False")
+		}
+	}
+}
