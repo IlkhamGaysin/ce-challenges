0c67c76545c5fac2967fb2f7096234bf011a701f
Nicolas Kaiser <nikai@nikai.net>
2015-10-10 10:56:39 +0200

    improve Number of Ones
    C

--- moderate/num_of_ones.c
+++ moderate/num_of_ones.c
@@ -3,13 +3,10 @@
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	int a, i;
+	unsigned a;
 
 	fp = fopen(*++argv, "r");
-	while (fscanf(fp, "%d", &a) != EOF) {
-		for (i = 0; a > 0; a &= a - 1)
-			i++;
-		printf("%d\n", i);
-	}
+	while (fscanf(fp, "%d", &a) != EOF)
+		printf("%d\n", __builtin_popcount(a));
 	return 0;
 }
