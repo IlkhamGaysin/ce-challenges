6ffda61cf3f60146f8c17f3110efddd91d7dc1c7
Nicolas Kaiser <nikai@nikai.net>
2015-10-10 11:46:01 +0200

    add One zero, two zero...
    C

---
+++ easy/one_zero_two_zero.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool xz(unsigned n, unsigned a) {
+	while (a) {
+		if (a % 2 == 0) {
+			if (n)
+				n--;
+			else
+				return false;
+		}
+		a >>= 1;
+	}
+	return n == 0;
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	unsigned nz, x, i;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d %d", &nz, &x) != EOF) {
+		unsigned r = 0;
+		for (i = 1; i <= x; i++)
+			if (xz(nz, i))
+				r++;
+		printf("%d\n", r);
+	}
+	return 0;
+}
