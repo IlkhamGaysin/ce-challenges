d12afffbff98e3f7bfa3adc5ce88798ec28ab597
Nicolas Kaiser <nikai@nikai.net>
2015-10-11 20:56:27 +0200

    more One zero, two zero...
    Haskell

---
+++ easy/one_zero_two_zero.hs
@@ -0,0 +1,22 @@
+import System.Environment (getArgs)
+
+toTuple       :: [Int] -> (Int, Int)
+toTuple [x, y] = (x, y)
+
+haszero    :: Int -> Int -> Bool
+haszero 0 0 = True
+haszero _ 0 = False
+haszero x y | mod y 2 == 1 = haszero x (div y 2)
+            | x == 0       = False
+            | otherwise    = haszero (pred x) (div y 2)
+
+onezero       :: (Int, Int) -> Int
+onezero (_, 0) = 0
+onezero (x, y) | haszero x y = succ $ onezero (x, pred y)
+               | otherwise   = onezero (x, pred y)
+
+main :: IO ()
+main = do
+    [inpFile] <- getArgs
+    input <- readFile inpFile
+    putStr . unlines . map (show . onezero . toTuple . map read . words) $ lines input
