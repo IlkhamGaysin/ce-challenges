c9bf8d1b2345747e084cecd66dea4bcea9413d2e
Nicolas Kaiser <nikai@nikai.net>
2015-10-13 16:35:56 +0200

    more One zero, two zero...
    Ruby

---
+++ easy/one_zero_two_zero.rb
@@ -0,0 +1,12 @@
+File.open(ARGV[0]).each_line do |line|
+  s, r = line.split.map(&:to_i), 0
+  1.upto(s[1]) do |i|
+    n, a = s[0], i
+    while a > 0 and n >= 0 do
+      n -= 1 if a % 2 == 0
+      a /= 2
+    end
+    r += 1 if n == 0
+  end
+  puts r
+end
