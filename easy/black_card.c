86f55637d842fa6712e34a2aa3d4d86365767a48
Nicolas Kaiser <nikai@nikai.net>
2015-11-16 12:24:22 +0100

    more Black card
    C

---
+++ easy/black_card.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int sbs = 64, w[11] = {0}, i = 0, j, k, m = 0, n;
+	char c, *sb = malloc(sbs);
+
+	fp = fopen(*++argv, "r");
+	while ((c = getc(fp)) != EOF) {
+		if (c == '|') {
+			fscanf(fp, " %d\n", &n);
+			while (m > 1) {
+				k = n % m - 1;
+				if (k >= 0) {
+					for (j = k; j < m - 1; j++)
+						w[j] = w[j + 1];
+				}
+				m--;
+			}
+			puts(sb + w[0]);
+			i = 0;
+			m = 0;
+			w[0] = 0;
+		} else {
+			if (i == sbs) {
+				sbs += sbs / 2;
+				sb = realloc(sb, sbs);
+			}
+			if (c == ' ') {
+				sb[i++] = '\0';
+				w[++m] = i;
+			} else {
+				sb[i++] = c;
+			}
+		}
+	}
+	free(sb);
+	return 0;
+}
