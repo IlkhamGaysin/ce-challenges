84c73a9eaa16c362232108f3eaa77c65c658478e
Nicolas Kaiser <nikai@nikai.net>
2015-11-21 22:35:42 +0100

    more Alternative reality
    C

---
+++ moderate/alternative_reality.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+const int m[5] = {50, 25, 10, 5, 1};
+
+int alter(int n, int p) {
+	if (n == 0)
+		return 1;
+	while (m[p] > n)
+		p++;
+	if (m[p] == 1)
+		return 1;
+	return alter(n - m[p], p) + alter(n, p + 1);
+}
+
+int main(int argc, char *argv[])
+{
+	FILE *fp;
+	int n;
+
+	fp = fopen(*++argv, "r");
+	while (fscanf(fp, "%d;", &n) != EOF)
+		printf("%d\n", alter(n, 0));
+	return 0;
+}
