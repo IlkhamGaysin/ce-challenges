b92c2a0c902c8fa18a00d742ac94010df730dd33
Nicolas Kaiser <nikai@nikai.net>
2015-12-04 11:31:27 +0100

    more Chain Inspection
    Python 2, Python 3, and improve Ruby

--- moderate/chain_inspection.rb
+++ moderate/chain_inspection.rb
@@ -4,7 +4,7 @@ File.open(ARGV[0]).each_line do |line|
     t = x.split("-")
     m[t[0]] = t[1]
   end
-  s.length.times do |x|
+  s.length.times do
     if not m.has_key?(b) or been.has_key?(m[b])
       b = nil
       break
