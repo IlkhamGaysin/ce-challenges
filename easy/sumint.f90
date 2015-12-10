52c981080eec2ddfc67b44b7f7d129088396687a
Nicolas Kaiser <nikai@nikai.net>
2015-12-10 22:40:06 +0100

    more Sum of Integers from File
    Fortran, and improve Decimal To Binary

--- easy/sumint.f90
+++ easy/sumint.f90
@@ -1,17 +1,20 @@
-program dec2bin
+program sumint
   character(32) :: arg
   character(10) :: str
   integer :: stat
+  integer :: i
   integer :: n
+  n = 0
   call get_command_argument(1, arg)
   open(11, file=trim(arg), action='read')
   do
     read(11, '(a)', iostat=stat) str
     if (stat /= 0) exit
     if (str /= '') then
-      read(str, '(i10)') n
-      write(*,'(b0)') n
+      read(str, '(i10)') i
+      n = n + i
     end if
   end do
+  write(*,'(i0)') n
   close(11)
-end program dec2bin
+end program sumint
