6e585e1ea7880162aea294724b741b8fb51cd3ca
Nicolas Kaiser <nikai@nikai.net>
2015-12-10 22:50:16 +0100

    more File Size
    Fortran

---
+++ easy/filesize.f90
@@ -0,0 +1,7 @@
+program filesize
+  character(32) :: arg
+  integer, dimension(13) :: buff
+  call get_command_argument(1, arg)
+  call stat(trim(arg), buff)
+  write(*, '(i0)') buff(8)
+end program filesize
