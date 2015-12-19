84e31e805d001b436a84215dd64e01bb3252608b
Nicolas Kaiser <nikai@nikai.net>
2015-12-19 23:20:19 +0100

    more Real fake
    Bash, Ruby, and improve C

--- easy/real_fake.c
+++ easy/real_fake.c
@@ -5,24 +5,21 @@ const char *m[] = {"Fake", "Real"};
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	int i = 0, e = 0, o = 0;
+	int i = 0, e = 0;
 	char c;
 
 	fp = fopen(*++argv, "r");
 	while ((c = getc(fp)) != EOF || i) {
 		if (c == '\n' || c == EOF) {
-			printf("%s\n", m[(i % 2 ? o : e) % 10 == 0]);
+			printf("%s\n", m[e % 10 == 0]);
 			i = 0;
 			e = 0;
-			o = 0;
 		} else if (c != ' ') {
 			int x = c - '0';
-			e += x;
-			o += x;
-			if (i % 2)
-				o += x;
-			else
+			if (i & 1)
 				e += x;
+			else
+				e += x * 2;
 			i++;
 		}
 	}
