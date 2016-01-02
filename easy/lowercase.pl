1f05df35088ab998d09c1aa45c79a7e0f8a6d003
Nicolas Kaiser <nikai@nikai.net>
2016-01-02 23:41:09 +0100

    more Lowercase, N Mod M
    Perl

---
+++ easy/lowercase.pl
@@ -0,0 +1,8 @@
+open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+while(my $line = <INFILE>) {
+    next if($line =~ m/^\s$/);
+    chomp($line);
+    $line = lc $line;
+    print "$line\n";
+}
+close(INFILE);
