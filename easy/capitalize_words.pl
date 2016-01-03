6bf663c924c17208872782480d2f4d0b27b17005
Nicolas Kaiser <nikai@nikai.net>
2016-01-03 23:54:09 +0100

    more Capitalize Words, Sum of Digits
    Perl

---
+++ easy/capitalize_words.pl
@@ -0,0 +1,8 @@
+open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+while(my $line = <INFILE>) {
+    next if($line =~ m/^\s$/);
+    chomp($line);
+    my @r = map {ucfirst $_} split(/ /, $line);
+    printf("%s\n", join(' ', @r));
+}
+close(INFILE);
---
+++ easy/sum_digits.pl
@@ -0,0 +1,9 @@
+open(INFILE, "<", $ARGV[0]) or die("Cannot open file $ARGV[0] for reading: $!");
+while(my $line = <INFILE>) {
+    next if($line =~ m/^\s$/);
+    chomp($line);
+    my $r;
+    map { $r += $_ } split(//, $line);
+    print "$r\n";
+}
+close(INFILE);
