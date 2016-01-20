d16de00d2ec6c4a551ec09e37470b8db4bec6f00
Nicolas Kaiser <nikai@nikai.net>
2016-01-20 17:06:55 +0100

    more Reverse words
    Perl, and improve Penultimate Word

--- easy/reverse_words.pl
+++ easy/reverse_words.pl
@@ -3,7 +3,8 @@ open( INFILE, "<", $ARGV[0] )
   or die("Cannot open file $ARGV[0] for reading: $!");
 while ( my $line = <INFILE> ) {
     next if ( $line =~ m/^\s$/ );
-    my @s = split( / /, $line );
-    print "$s[-2]\n";
+    chomp($line);
+    my @r = reverse split( / /, $line );
+    printf( "%s\n", join( ' ', @r ) );
 }
 close(INFILE);
