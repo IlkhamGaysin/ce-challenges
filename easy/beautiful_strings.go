55b31abd7936184dd352e6b44267c35fa6139de7
Nicolas Kaiser <nikai@nikai.net>
2016-05-07 21:10:07 +0200

    refactor Beautiful Strings
    Go, and add test and benchmark

--- easy/beautiful_strings.go
+++ easy/beautiful_strings.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+var c []int
+
+func init() {
+	c = make([]int, 26)
+}
+
 func clean(char rune) rune {
 	if char >= 'a' && char <= 'z' {
 		return char
@@ -16,8 +22,20 @@ func clean(char rune) rune {
 	return -1
 }
 
+func beauty(s string) (r int) {
+	t := strings.Map(clean, strings.ToLower(s))
+	for _, i := range t {
+		c[i-'a']++
+	}
+	sort.Ints(c)
+	for i := 26; i > 0 && c[i-1] > 0; i-- {
+		r += i * c[i-1]
+		c[i-1] = 0
+	}
+	return r
+}
+
 func main() {
-	c := make([]int, 26)
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -25,16 +43,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		t := strings.Map(clean, strings.ToLower(scanner.Text()))
-		for _, i := range t {
-			c[i-'a']++
-		}
-		sort.Ints(c)
-		r := 0
-		for i := 26; i > 0 && c[i-1] > 0; i-- {
-			r += i * c[i-1]
-			c[i-1] = 0
-		}
-		fmt.Println(r)
+		fmt.Println(beauty(scanner.Text()))
 	}
 }
