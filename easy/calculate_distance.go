67587ba2c172b3842d7270cbe0133c4f1e6636a4
Nicolas Kaiser <nikai@nikai.net>
2016-05-09 12:22:05 +0200

    refactor Calculate Distance
    Go

--- easy/calculate_distance.go
+++ easy/calculate_distance.go
@@ -8,7 +8,12 @@ import (
 	"os"
 )
 
+func sqrt(a int) int {
+	return int(math.Sqrt(float64(a)))
+}
+
 func main() {
+	var x1, y1, x2, y2 int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -16,9 +21,8 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var x1, y1, x2, y2 int
 		fmt.Sscanf(scanner.Text(), "(%d, %d) (%d, %d)", &x1, &y1, &x2, &y2)
 		x, y := x1-x2, y1-y2
-		fmt.Println(int(math.Sqrt(float64(x*x + y*y))))
+		fmt.Println(sqrt(x*x + y*y))
 	}
 }
