e23ced9b92b7d5cf8ebcb5e1a5bf21b154134c1b
Nicolas Kaiser <nikai@nikai.net>
2016-05-17 21:48:35 +0200

    refactor Lucky tickets
    Go, and add test and benchmark

--- moderate/lucky_tickets.go
+++ moderate/lucky_tickets.go
@@ -8,9 +8,30 @@ import (
 	"os"
 )
 
+var r, b, c *big.Int
+
+func init() {
+	r, b, c = new(big.Int), new(big.Int), new(big.Int)
+}
+
+func lucky(m int64) *big.Int {
+	a := true
+	r.SetInt64(0)
+	for i := int64(0); i < m/2; i++ {
+		b.Binomial(m, i)
+		c.Binomial(11*m/2-1-10*i, m-1)
+		if a {
+			r.Add(r, b.Mul(b, c))
+		} else {
+			r.Sub(r, b.Mul(b, c))
+		}
+		a = !a
+	}
+	return r
+}
+
 func main() {
-	var m, i int64
-	r, b, c := new(big.Int), new(big.Int), new(big.Int)
+	var m int64
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -19,20 +40,6 @@ func main() {
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
 		fmt.Sscanf(scanner.Text(), "%d", &m)
-		a := true
-
-		r.SetInt64(0)
-		for i = 0; i < m/2; i++ {
-			b.Binomial(m, i)
-			c.Binomial(11*m/2-1-10*i, m-1)
-			if a {
-				r.Add(r, b.Mul(b, c))
-			} else {
-				r.Sub(r, b.Mul(b, c))
-			}
-			a = !a
-		}
-
-		fmt.Println(r)
+		fmt.Println(lucky(m))
 	}
 }
