26d97ffa7526b40da3ce989171e434d054820dcd
Nicolas Kaiser <nikai@nikai.net>
2016-06-06 16:33:06 +0200

    refactor The Major Element
    Go, and add test and benchmark

--- easy/major_element.go
+++ easy/major_element.go
@@ -5,9 +5,27 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 	"strings"
 )
+
+func major(s string) string {
+	var k, maxnum, maxcount int
+	num := make([]int, 101)
+	t := strings.Split(s, ",")
+	for ix, i := range t {
+		fmt.Sscan(i, &k)
+		num[k]++
+		if num[k] > maxcount {
+			maxnum, maxcount = k, num[k]
+		}
+		if len(t)/2+1 > maxcount+len(t)-ix-1 {
+			break
+		} else if maxcount >= len(t)/2+1 {
+			return fmt.Sprint(maxnum)
+		}
+	}
+	return "None"
+}
 
 func main() {
 	data, err := os.Open(os.Args[1])
@@ -17,25 +35,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var maxnum, maxcount int
-		num := make([]int, 101)
-		sequ := strings.Split(strings.TrimSpace(scanner.Text()), ",")
-		for ix, i := range sequ {
-			k, err := strconv.Atoi(i)
-			if err != nil {
-				log.Fatal(err)
-			}
-			num[k]++
-			if num[k] > maxcount {
-				maxnum, maxcount = k, num[k]
-			}
-			if len(sequ)/2+1 > maxcount+len(sequ)-ix-1 {
-				fmt.Println("None")
-				break
-			} else if maxcount >= len(sequ)/2+1 {
-				fmt.Println(maxnum)
-				break
-			}
-		}
+		fmt.Println(major(scanner.Text()))
 	}
 }
