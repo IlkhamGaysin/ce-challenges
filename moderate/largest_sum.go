a817e30478c4484a17d2ac88ac0f8a7e74d8fa13
Nicolas Kaiser <nikai@nikai.net>
2016-06-18 14:33:37 +0200

    refactor Sum of integers
    Go, and add test

--- moderate/largest_sum.go
+++ moderate/largest_sum.go
@@ -8,6 +8,24 @@ import (
 	"strings"
 )
 
+func largestSum(q string) int {
+	s := strings.Split(q, ",")
+	var l, m, v int
+	fmt.Sscan(strings.TrimSpace(s[0]), &m)
+	for _, i := range s {
+		fmt.Sscan(strings.TrimSpace(i), &v)
+		if v+l > m {
+			m = v + l
+		}
+		if v+l > 0 {
+			l += v
+		} else {
+			l = 0
+		}
+	}
+	return m
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -16,21 +34,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		s := strings.Split(scanner.Text(), ",")
-		var l, m int
-		fmt.Sscanf(strings.TrimSpace(s[0]), "%d", &m)
-		for _, i := range s {
-			var v int
-			fmt.Sscanf(strings.TrimSpace(i), "%d", &v)
-			if v+l > m {
-				m = v + l
-			}
-			if v+l > 0 {
-				l += v
-			} else {
-				l = 0
-			}
-		}
-		fmt.Println(m)
+		fmt.Println(largestSum(scanner.Text()))
 	}
 }
