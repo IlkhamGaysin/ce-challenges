c689ff0c90bd75ac36c7e0afc8a13551cf4658d4
Nicolas Kaiser <nikai@nikai.net>
2016-06-23 21:39:17 +0200

    add test to Point in Circle
    Go

--- moderate/point_in_circle.go
+++ moderate/point_in_circle.go
@@ -12,6 +12,7 @@ func inCircle(a, b, c float64) bool {
 }
 
 func main() {
+	f := make([]float64, 5)
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -19,7 +20,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		f := make([]float64, 5)
 		fmt.Sscanf(scanner.Text(), "Center: (%f, %f); Radius: %f; Point: (%f, %f)",
 			&f[0], &f[1], &f[2], &f[3], &f[4])
 		fmt.Println(inCircle(f[0]-f[3], f[1]-f[4], f[2]))
