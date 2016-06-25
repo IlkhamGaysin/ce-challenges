8ec03ccbeaf6c933ac2fee58e14bc3ad565cd141
Nicolas Kaiser <nikai@nikai.net>
2016-06-25 12:47:47 +0200

    refactor Delta Time
    Go, and add test and benchmark

--- easy/delta_time.go
+++ easy/delta_time.go
@@ -14,16 +14,20 @@ func abs(a int) int {
 	return a
 }
 
+func delta(h1, m1, s1, h2, m2, s2 int) string {
+	t := abs((h1-h2)*3600 + (m1-m2)*60 + s1 - s2)
+	return fmt.Sprintf("%02d:%02d:%02d", t/3600, (t/60)%60, t%60)
+}
+
 func main() {
+	var h1, m1, s1, h2, m2, s2 int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var h1, m1, s1, h2, m2, s2 int
 		fmt.Sscanf(scanner.Text(), "%d:%d:%d %d:%d:%d", &h1, &m1, &s1, &h2, &m2, &s2)
-		t := abs((h1-h2)*3600 + (m1-m2)*60 + s1 - s2)
-		fmt.Printf("%02d:%02d:%02d\n", t/3600, (t/60)%60, t%60)
+		fmt.Println(delta(h1, m1, s1, h2, m2, s2))
 	}
 }
