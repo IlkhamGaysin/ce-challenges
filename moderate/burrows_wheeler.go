de603e83934d2b8ab3e92e8040734fbb5aa00c4e
Nicolas Kaiser <nikai@nikai.net>
2016-06-27 16:47:32 +0200

    refactor Burrows-Wheeler transform
    Go, and add test

--- moderate/burrows_wheeler.go
+++ moderate/burrows_wheeler.go
@@ -9,6 +9,22 @@ import (
 	"strings"
 )
 
+func burrowsWheeler(s string) string {
+	e, r := make([]int, len(s)), make([]byte, len(s))
+	var k int
+	for i := 0; i < len(s); i++ {
+		e[i] = int(s[i])<<16 + i
+		if s[i] == '$' {
+			k = i
+		}
+	}
+	sort.Ints(e)
+	for i := 0; i < len(s); i, k = i+1, e[k]%(1<<16) {
+		r[i] = byte(e[k] >> 16)
+	}
+	return string(r)
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -17,19 +33,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		s := strings.TrimRight(scanner.Text(), "|")
-		e := make([]int, len(s))
-		var k int
-		for i := 0; i < len(s); i++ {
-			e[i] = int(s[i])<<16 + i
-			if s[i] == '$' {
-				k = i
-			}
-		}
-		sort.Ints(e)
-		for i := 0; i < len(s); i, k = i+1, e[k]%(1<<16) {
-			fmt.Printf("%c", e[k]>>16)
-		}
-		fmt.Println()
+		fmt.Println(burrowsWheeler(strings.TrimRight(scanner.Text(), "|")))
 	}
 }
