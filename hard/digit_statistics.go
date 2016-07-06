6a564fed98a6ca8df5b6dff5bf35b8a2b15d0160
Nicolas Kaiser <nikai@nikai.net>
2016-07-06 17:18:18 +0200

    refactor Digit statistics
    Go, and add test

--- hard/digit_statistics.go
+++ hard/digit_statistics.go
@@ -8,12 +8,33 @@ import (
 	"strings"
 )
 
-func main() {
-	stats := [][]int{[]int{2, 4, 8, 6}, []int{3, 9, 7, 1},
+var stats [][]int
+
+func init() {
+	stats = [][]int{[]int{2, 4, 8, 6}, []int{3, 9, 7, 1},
 		[]int{4, 6}, []int{5}, []int{6},
 		[]int{7, 9, 3, 1}, []int{8, 4, 2, 6},
 		[]int{9, 1}}
+}
 
+func digitStatistics(a, n int) string {
+	a -= 2
+	m := n / len(stats[a])
+	q, r := make([]int, 10), make([]string, 10)
+	for _, i := range stats[a] {
+		q[i] += m
+	}
+	for i := 0; i < n%len(stats[a]); i++ {
+		q[stats[a][i]]++
+	}
+	for i := 0; i < 10; i++ {
+		r[i] = fmt.Sprintf("%d: %d", i, q[i])
+	}
+	return strings.Join(r, ", ")
+}
+
+func main() {
+	var a, n int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -21,21 +42,7 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var a, n int
 		fmt.Sscanf(scanner.Text(), "%d %d", &a, &n)
-		a -= 2
-		m := n / len(stats[a])
-		res := make([]int, 10)
-		for _, i := range stats[a] {
-			res[i] += m
-		}
-		for i := 0; i < n%len(stats[a]); i++ {
-			res[stats[a][i]]++
-		}
-		resu := make([]string, 10)
-		for i := 0; i < 10; i++ {
-			resu[i] = fmt.Sprintf("%d: %d", i, res[i])
-		}
-		fmt.Println(strings.Join(resu, ", "))
+		fmt.Println(digitStatistics(a, n))
 	}
 }
