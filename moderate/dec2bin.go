5a85e77664a2681e95c414cf0c8508243399c425
Nicolas Kaiser <nikai@nikai.net>
2016-07-08 17:24:29 +0200

    refactor Decimal To Binary
    Go, and add test

--- moderate/dec2bin.go
+++ moderate/dec2bin.go
@@ -7,8 +7,12 @@ import (
 	"os"
 )
 
+func dec2bin(a uint) string {
+	return fmt.Sprintf("%b", a)
+}
+
 func main() {
-	var t int
+	var a uint
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -16,10 +20,9 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		if len(scanner.Text()) == 0 {
-			continue
+		if len(scanner.Text()) > 0 {
+			fmt.Sscan(scanner.Text(), &a)
+			fmt.Println(dec2bin(a))
 		}
-		fmt.Sscanf(scanner.Text(), "%d", &t)
-		fmt.Printf("%b\n", t)
 	}
 }
