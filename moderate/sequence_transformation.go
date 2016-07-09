a09614fd8a06a56c2f860ed9f7c690b489f2e734
Nicolas Kaiser <nikai@nikai.net>
2016-07-09 23:36:14 +0200

    refactor Sequence Transformation
    Go, and add test

--- moderate/sequence_transformation.go
+++ moderate/sequence_transformation.go
@@ -9,6 +9,12 @@ import (
 	"strings"
 )
 
+func seqTrans(p, q string) bool {
+	p = "^" + strings.Replace(strings.Replace(p, "0", "A+", -1), "1", "((A+)|(B+))", -1) + "$"
+	seqRegex := regexp.MustCompile(p)
+	return seqRegex.MatchString(q)
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -18,9 +24,7 @@ func main() {
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
 		s := strings.Fields(scanner.Text())
-		s[0] = "^" + strings.Replace(strings.Replace(s[0], "0", "A+", -1), "1", "((A+)|(B+))", -1) + "$"
-		seqRegex := regexp.MustCompile(s[0])
-		if seqRegex.MatchString(s[1]) {
+		if seqTrans(s[0], s[1]) {
 			fmt.Println("Yes")
 		} else {
 			fmt.Println("No")
