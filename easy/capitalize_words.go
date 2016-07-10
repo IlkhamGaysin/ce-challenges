0db1e396120637ee8e80c4388c33d1d4a6720f69
Nicolas Kaiser <nikai@nikai.net>
2016-07-10 18:19:57 +0200

    refactor Capitalize Words
    Go, and add test

--- easy/capitalize_words.go
+++ easy/capitalize_words.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+func capitalize(q string) string {
+	return strings.Title(q)
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -16,6 +20,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		fmt.Println(strings.Title(strings.TrimSpace(scanner.Text())))
+		fmt.Println(capitalize(scanner.Text()))
 	}
 }
