599b652fede8ccf9ff191fecd1acc1ac60a270e0
Nicolas Kaiser <nikai@nikai.net>
2016-07-13 12:35:51 +0200

    fix and refactor Simple Sorting
    Go, and add test

--- easy/simple_sorting.go
+++ easy/simple_sorting.go
@@ -9,18 +9,20 @@ import (
 	"strings"
 )
 
-type number struct {
-	f float64
-	s string
+func simpleSort(q string) string {
+	t := strings.Fields(q)
+	u := make([]float64, len(t))
+	for ix, i := range t {
+		fmt.Sscan(i, &u[ix])
+	}
+	sort.Float64s(u)
+	for ix, i := range u {
+		t[ix] = fmt.Sprintf("%.3f", i)
+	}
+	return strings.Join(t, " ")
 }
-type numbers []number
-
-func (slice numbers) Len() int           { return len(slice) }
-func (slice numbers) Less(i, j int) bool { return slice[i].f < slice[j].f }
-func (slice numbers) Swap(i, j int)      { slice[i], slice[j] = slice[j], slice[i] }
 
 func main() {
-	var f float64
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -28,19 +30,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var nums []number
-		t := strings.Fields(scanner.Text())
-		for _, i := range t {
-			fmt.Sscan(i, &f)
-			nums = append(nums, number{f, i})
-		}
-		sort.Sort(numbers(nums))
-		for ix, i := range nums {
-			if ix > 0 {
-				fmt.Print(" ")
-			}
-			fmt.Print(i.s)
-		}
-		fmt.Println()
+		fmt.Println(simpleSort(scanner.Text()))
 	}
 }
