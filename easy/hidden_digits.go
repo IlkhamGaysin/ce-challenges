a3db8db72245254198f7438c57797bfb1ab8dee1
Nicolas Kaiser <nikai@nikai.net>
2016-07-14 12:46:17 +0200

    refactor Hidden Digits
    Go, and add test

--- easy/hidden_digits.go
+++ easy/hidden_digits.go
@@ -8,6 +8,22 @@ import (
 	"strings"
 )
 
+func hiddenDigits(q string) string {
+	maph := func(r rune) rune {
+		if strings.Contains("0123456789", string(r)) {
+			return r
+		} else if strings.Contains("abcdefghij", string(r)) {
+			return r - 'a' + '0'
+		}
+		return -1
+	}
+	if r := strings.Map(maph, q); len(r) > 0 {
+		return r
+	} else {
+		return "NONE"
+	}
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -16,20 +32,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		maph := func(r rune) rune {
-			if strings.Contains("0123456789", string(r)) {
-				return r
-			} else if strings.Contains("abcdefghij", string(r)) {
-				return r - 'a' + '0'
-			}
-			return -1
-		}
-		t := strings.Map(maph, scanner.Text())
-
-		if len(t) == 0 {
-			fmt.Println("NONE")
-		} else {
-			fmt.Println(t)
-		}
+		fmt.Println(hiddenDigits(scanner.Text()))
 	}
 }
