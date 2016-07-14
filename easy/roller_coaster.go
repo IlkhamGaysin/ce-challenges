02c096adb7ea870aec3d6db938bef9913b34a89e
Nicolas Kaiser <nikai@nikai.net>
2016-07-14 16:56:51 +0200

    refactor Roller Coaster
    Go, and add test

--- easy/roller_coaster.go
+++ easy/roller_coaster.go
@@ -7,35 +7,32 @@ import (
 	"os"
 )
 
+func rollerCoaster(q string) string {
+	var u bool
+	r := make([]byte, len(q))
+	for ix, i := range q {
+		if (i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') {
+			u = !u
+			if u {
+				r[ix] = byte(i) & 223
+			} else {
+				r[ix] = byte(i) | 32
+			}
+		} else {
+			r[ix] = byte(i)
+		}
+	}
+	return string(r)
+}
+
 func main() {
-	var (
-		u bool
-		c byte = '\n'
-	)
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
-	scanner.Split(bufio.ScanBytes)
 	for scanner.Scan() {
-		c = scanner.Text()[0]
-		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') {
-			u = !u
-			if u {
-				c &= 223
-			} else {
-				c |= 32
-			}
-		} else {
-			if c == '\n' {
-				u = false
-			}
-		}
-		fmt.Print(string(c))
-	}
-	if c != '\n' {
-		fmt.Println()
+		fmt.Println(rollerCoaster(scanner.Text()))
 	}
 }
