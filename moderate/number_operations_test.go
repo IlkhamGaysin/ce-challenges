8c17c908349c1615b2c93ad7c51a8482ee88602d
Nicolas Kaiser <nikai@nikai.net>
2016-07-17 16:16:14 +0200

    refactor Number Operations
    Go, and add test

--- moderate/number_operations_test.go
+++ moderate/number_operations_test.go
@@ -1,13 +1,48 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
 	"sort"
 	"strings"
+	"testing"
 )
+
+func TestNumberOperations(t *testing.T) {
+	for k, v := range map[string]bool{
+		"44 6 1 49 47":   false,
+		"34 1 49 2 21":   true,
+		"31 38 27 51 18": false} {
+		if r := numberOperations(k); r != v {
+			t.Errorf("failed: numberOperations %s is %t, got %t",
+				k, v, r)
+		}
+	}
+}
+
+func nextPermu(s string) (string, bool) {
+	k, l := -1, 0
+	for i := len(s) - 2; i >= 0; i-- {
+		if s[i] < s[i+1] {
+			k = i
+			break
+		}
+	}
+	if k == -1 {
+		return s, true
+	}
+	for i := len(s) - 1; i > 0; i-- {
+		if s[i] > s[k] {
+			l = i
+			break
+		}
+	}
+	s = strings.Join([]string{s[0:k], string(s[l]), s[k+1 : l], string(s[k]), s[l+1 : len(s)]}, "")
+	l = len(s) - k - 1
+	for i := 0; l-1-i > i; i++ {
+		s = strings.Join([]string{s[0 : k+1+i], string(s[len(s)-1-i]), s[k+2+i : len(s)-1-i], string(s[k+1+i]), s[len(s)-i : len(s)]}, "")
+	}
+	return s, false
+}
 
 func numberOperations(q string) bool {
 	s := strings.Fields(q)
@@ -74,45 +109,4 @@ func numberOperations(q string) bool {
 		v, f = nextPermu(v)
 	}
 	return g
-}
-
-func nextPermu(s string) (string, bool) {
-	k, l := -1, 0
-	for i := len(s) - 2; i >= 0; i-- {
-		if s[i] < s[i+1] {
-			k = i
-			break
-		}
-	}
-	if k == -1 {
-		return s, true
-	}
-	for i := len(s) - 1; i > 0; i-- {
-		if s[i] > s[k] {
-			l = i
-			break
-		}
-	}
-	s = strings.Join([]string{s[0:k], string(s[l]), s[k+1 : l], string(s[k]), s[l+1 : len(s)]}, "")
-	l = len(s) - k - 1
-	for i := 0; l-1-i > i; i++ {
-		s = strings.Join([]string{s[0 : k+1+i], string(s[len(s)-1-i]), s[k+2+i : len(s)-1-i], string(s[k+1+i]), s[len(s)-i : len(s)]}, "")
-	}
-	return s, false
-}
-
-func main() {
-	data, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer data.Close()
-	scanner := bufio.NewScanner(data)
-	for scanner.Scan() {
-		if numberOperations(scanner.Text()) {
-			fmt.Println("YES")
-		} else {
-			fmt.Println("NO")
-		}
-	}
 }
