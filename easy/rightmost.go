147e42eb8a4f4c7d197032364f63b341b48e0f3b
Nicolas Kaiser <nikai@nikai.net>
2016-07-19 16:37:12 +0200

    refactor Rightmost Char
    Go, and add test

--- easy/rightmost.go
+++ easy/rightmost.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+func rightmost(p, q string) int {
+	return strings.LastIndex(p, q)
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -17,7 +21,7 @@ func main() {
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
 		if s := strings.Split(scanner.Text(), ","); len(s) > 1 {
-			fmt.Println(strings.LastIndex(s[0], s[1]))
+			fmt.Println(rightmost(s[0], s[1]))
 		}
 	}
 }
