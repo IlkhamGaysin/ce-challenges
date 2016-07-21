8d459ab17a179dccaa5bd70b6d9f881c84164cf4
Nicolas Kaiser <nikai@nikai.net>
2016-07-21 20:37:18 +0200

    refactor Read More
    Go, and add test

--- easy/read_more.go
+++ easy/read_more.go
@@ -7,6 +7,22 @@ import (
 	"os"
 )
 
+func readMore(q string) string {
+	if len(q) > 55 {
+		for i := 39; i >= 0; i-- {
+			if q[i] == ' ' {
+				q = q[:i]
+				break
+			}
+		}
+		if len(q) > 55 {
+			q = q[:40]
+		}
+		q += "... <Read More>"
+	}
+	return q
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -14,19 +30,6 @@ func main() {
 	}
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		line := scanner.Text()
-		if len(line) > 55 {
-			for i := 39; i >= 0; i-- {
-				if line[i] == ' ' {
-					line = line[:i]
-					break
-				}
-			}
-			if len(line) > 55 {
-				line = line[:40]
-			}
-			line += "... <Read More>"
-		}
-		fmt.Println(line)
+		fmt.Println(readMore(scanner.Text()))
 	}
 }
