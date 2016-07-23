baa3d8f79b6a2e3ea6055cd0965b58e23380e7dc
Nicolas Kaiser <nikai@nikai.net>
2016-07-23 22:06:18 +0200

    refactor Fizz Buzz
    Go, and add test

--- easy/fizzbuzz.go
+++ easy/fizzbuzz.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 type node struct {
@@ -16,7 +17,34 @@ func (a node) isEmpty() bool {
 	return len(a.value) == 0
 }
 
+func fizzbuzz(f, b, n uint) string {
+	var r []string
+	tail := node{value: "FB"}
+	list := &tail
+	for i, j := f-1, b-1; i > 0 || j > 0; i, j = (i+f-1)%f, (j+b-1)%b {
+		var v string
+		if i == 0 {
+			v = "F"
+		} else if j == 0 {
+			v = "B"
+		}
+		list = &node{v, list}
+	}
+	tail.next = list
+
+	for i := uint(1); i <= n; i++ {
+		if list.isEmpty() {
+			r = append(r, fmt.Sprint(i))
+		} else {
+			r = append(r, list.value)
+		}
+		list = list.next
+	}
+	return strings.Join(r, " ")
+}
+
 func main() {
+	var f, b, n uint
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -24,34 +52,7 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var fizz, buzz, n int
-		fmt.Sscanf(scanner.Text(), "%d %d %d", &fizz, &buzz, &n)
-
-		tail := node{value: "FB"}
-		list := &tail
-		for a, b := fizz-1, buzz-1; a > 0 || b > 0; a, b = (a+fizz-1)%fizz, (b+buzz-1)%buzz {
-			var value string
-			if a == 0 {
-				value = "F"
-			} else if b == 0 {
-				value = "B"
-			}
-			list = &node{value, list}
-		}
-		tail.next = list
-
-		for i := 1; i <= n; i++ {
-			if list.isEmpty() {
-				fmt.Print(i)
-			} else {
-				fmt.Print(list.value)
-			}
-			list = list.next
-			if i < n {
-				fmt.Print(" ")
-			} else {
-				fmt.Println()
-			}
-		}
+		fmt.Sscanf(scanner.Text(), "%d %d %d", &f, &b, &n)
+		fmt.Println(fizzbuzz(f, b, n))
 	}
 }
