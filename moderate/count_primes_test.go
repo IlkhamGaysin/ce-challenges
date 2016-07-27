b95b2b5b28cb277cf97a9e62458fe9af91e9262b
Nicolas Kaiser <nikai@nikai.net>
2016-07-27 15:23:30 +0200

    refactor Counting Primes
    Go, and add test

--- moderate/count_primes_test.go
+++ moderate/count_primes_test.go
@@ -1,11 +1,27 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-)
+import "testing"
+
+type tuple struct {
+	a, b uint
+}
+
+func TestCountPrimes(t *testing.T) {
+	for k, v := range map[tuple]uint{
+		tuple{2, 10}:  4,
+		tuple{24, 25}: 0,
+		tuple{20, 30}: 2,
+		tuple{31, 31}: 1,
+		tuple{31, 32}: 1,
+		tuple{30, 31}: 1,
+		tuple{29, 31}: 2,
+		tuple{29, 29}: 1} {
+		if r := countPrimes(k.a, k.b); r != v {
+			t.Errorf("failed: countPrimes %d,%d is %d, got %d",
+				k.a, k.b, r, v)
+		}
+	}
+}
 
 var primes = []uint{2, 3, 5, 7, 11, 13}
 
@@ -43,18 +59,4 @@ func countPrimes(a, b uint) (r uint) {
 		}
 	}
 	return r
-}
-
-func main() {
-	var a, b uint
-	data, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer data.Close()
-	scanner := bufio.NewScanner(data)
-	for scanner.Scan() {
-		fmt.Sscanf(scanner.Text(), "%d,%d", &a, &b)
-		fmt.Println(countPrimes(a, b))
-	}
 }
