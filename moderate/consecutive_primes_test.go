8774c08a9a33a7000d78cec0439c8c0c53d46be5
Nicolas Kaiser <nikai@nikai.net>
2016-07-30 15:23:41 +0200

    refactor Consecutive Primes
    Go, and add test and benchmark

--- moderate/consecutive_primes_test.go
+++ moderate/consecutive_primes_test.go
@@ -1,12 +1,27 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
 	"sort"
+	"testing"
 )
+
+func TestConsecutivePrimes(t *testing.T) {
+	for k, v := range map[int]uint{
+		3: 0, 5: 0, 7: 0, 9: 0, 11: 0, 13: 0, 15: 0, 17: 0,
+		2: 1, 4: 2, 6: 2, 8: 4, 10: 96, 12: 1024, 14: 2880,
+		16: 81024, 18: 770144} {
+		if r := consecutivePrimes(k); r != v {
+			t.Errorf("failed: consecutivePrimes %d is %d, got %d",
+				k, v, r)
+		}
+	}
+}
+
+func BenchmarkConsecutivePrimes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		consecutivePrimes(i%17 + 2)
+	}
+}
 
 var (
 	primes = []int{2, 3, 5, 7, 11, 13}
@@ -62,18 +77,4 @@ func consecutivePrimes(a int) uint {
 	}
 	d := (1 << uint(a+1)) - 4
 	return consecutive(g, d, 1)
-}
-
-func main() {
-	var n int
-	data, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer data.Close()
-	scanner := bufio.NewScanner(data)
-	for scanner.Scan() {
-		fmt.Sscan(scanner.Text(), &n)
-		fmt.Println(consecutivePrimes(n))
-	}
 }
