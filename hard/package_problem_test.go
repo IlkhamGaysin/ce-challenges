d3a80c7ffcc8c93fa52866f44dc9209383d93967
Nicolas Kaiser <nikai@nikai.net>
2016-08-07 17:40:05 +0200

    refactor Package Problem
    Go, and add test

--- hard/package_problem_test.go
+++ hard/package_problem_test.go
@@ -1,14 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
 	"math"
-	"os"
 	"sort"
 	"strings"
+	"testing"
 )
+
+func TestPackageProblem(t *testing.T) {
+	for k, v := range map[string]string{
+		"81 : (1,53.38,$45) (2,88.62,$98) (3,78.48,$3) (4,72.30,$76) (5,30.18,$9) (6,46.34,$48)": "4",
+		"8 : (1,15.3,$34)": "-",
+		"75 : (1,85.31,$29) (2,14.55,$74) (3,3.98,$16) (4,26.24,$55) (5,63.69,$52) (6,76.25,$75) (7,60.02,$74) (8,93.18,$35) (9,89.95,$78)": "2,7",
+		"56 : (1,90.72,$13) (2,33.80,$40) (3,43.15,$10) (4,37.97,$16) (5,46.81,$36) (6,48.77,$79) (7,81.80,$45) (8,19.36,$79) (9,6.76,$64)": "8,9"} {
+		if r := packageProblem(k); r != v {
+			t.Errorf("failed: packageProblem %s is %s, got %s",
+				k, v, r)
+		}
+	}
+}
 
 type item struct {
 	id, value, weight int64
@@ -145,18 +156,4 @@ func packageProblem(q string) string {
 		}
 	}
 	return strings.Join(st, ",")
-}
-
-func main() {
-	data, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer data.Close()
-	scanner := bufio.NewScanner(data)
-	for scanner.Scan() {
-		if len(scanner.Text()) > 2 {
-			fmt.Println(packageProblem(scanner.Text()))
-		}
-	}
 }
