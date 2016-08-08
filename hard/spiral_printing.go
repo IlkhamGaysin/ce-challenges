d4193b43e2cd1f918d21a4f0e9dc38f647b0eec8
Nicolas Kaiser <nikai@nikai.net>
2016-08-08 22:49:24 +0200

    refactor Spiral Printing
    Go, and add test

--- hard/spiral_printing.go
+++ hard/spiral_printing.go
@@ -8,8 +8,60 @@ import (
 	"strings"
 )
 
+func spiral(a, b int, q string) string {
+	var (
+		r            []string
+		i, j, tn, tw int
+	)
+	t := strings.Fields(q)
+	te, ts := b-1, a-1
+	for {
+		for j <= te {
+			r = append(r, t[i*b+j])
+			j++
+		}
+		j--
+		i++
+		tn++
+		if i > ts {
+			break
+		}
+		for i <= ts {
+			r = append(r, t[i*b+j])
+			i++
+		}
+		i--
+		j--
+		te--
+		if j < tw {
+			break
+		}
+		for j >= tw {
+			r = append(r, t[i*b+j])
+			j--
+		}
+		j++
+		i--
+		ts--
+		if i < tn {
+			break
+		}
+		for i >= tn {
+			r = append(r, t[i*b+j])
+			i--
+		}
+		i++
+		j++
+		tw++
+		if j > te {
+			break
+		}
+	}
+	return strings.Join(r, " ")
+}
+
 func main() {
-	var row, col int
+	var a, b int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -17,57 +69,9 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var (
-			r            []string
-			i, j, tn, tw int
-		)
 		s := strings.Split(scanner.Text(), ";")
-		fmt.Sscanf(s[0], "%d", &row)
-		fmt.Sscanf(s[1], "%d", &col)
-		t := strings.Fields(s[2])
-		te, ts := col-1, row-1
-		for {
-			for j <= te {
-				r = append(r, t[i*col+j])
-				j++
-			}
-			j--
-			i++
-			tn++
-			if i > ts {
-				break
-			}
-			for i <= ts {
-				r = append(r, t[i*col+j])
-				i++
-			}
-			i--
-			j--
-			te--
-			if j < tw {
-				break
-			}
-			for j >= tw {
-				r = append(r, t[i*col+j])
-				j--
-			}
-			j++
-			i--
-			ts--
-			if i < tn {
-				break
-			}
-			for i >= tn {
-				r = append(r, t[i*col+j])
-				i--
-			}
-			i++
-			j++
-			tw++
-			if j > te {
-				break
-			}
-		}
-		fmt.Println(strings.Join(r, " "))
+		fmt.Sscan(s[0], &a)
+		fmt.Sscan(s[1], &b)
+		fmt.Println(spiral(a, b, s[2]))
 	}
 }
