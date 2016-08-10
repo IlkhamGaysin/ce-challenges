45473f428910058a3aadcf4fbe563092a4ffe435
Nicolas Kaiser <nikai@nikai.net>
2016-08-10 21:24:05 +0200

    refactor Filename Pattern
    Go, and add test

--- moderate/filename.go
+++ moderate/filename.go
@@ -9,6 +9,28 @@ import (
 	"strings"
 )
 
+func filename(q string) string {
+	var r []string
+	f := strings.Fields(q)
+	p := regexp.QuoteMeta(f[0])
+	p = strings.Replace(p, `\?`, ".", -1)
+	p = strings.Replace(p, `\*`, ".*", -1)
+	p = strings.Replace(p, `\[`, "[", -1)
+	p = strings.Replace(p, `\]`, "]", -1)
+	p = "^" + p + "$"
+	pattern := regexp.MustCompile(p)
+	for i := 1; i < len(f); i++ {
+		if pattern.MatchString(f[i]) {
+			r = append(r, f[i])
+		}
+	}
+	if len(r) > 0 {
+		return strings.Join(r, " ")
+	} else {
+		return "-"
+	}
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -17,24 +39,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var r []string
-		f := strings.Fields(scanner.Text())
-		p := regexp.QuoteMeta(f[0])
-		p = strings.Replace(p, `\?`, ".", -1)
-		p = strings.Replace(p, `\*`, ".*", -1)
-		p = strings.Replace(p, `\[`, "[", -1)
-		p = strings.Replace(p, `\]`, "]", -1)
-		p = "^" + p + "$"
-		pattern := regexp.MustCompile(p)
-		for i := 1; i < len(f); i++ {
-			if pattern.MatchString(f[i]) {
-				r = append(r, f[i])
-			}
-		}
-		if len(r) > 0 {
-			fmt.Println(strings.Join(r, " "))
-		} else {
-			fmt.Println("-")
-		}
+		fmt.Println(filename(scanner.Text()))
 	}
 }
