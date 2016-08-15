0cc734e82d62ffdfcd952b75746225cc0b00746b
Nicolas Kaiser <nikai@nikai.net>
2016-08-15 20:58:02 +0200

    fix and refactor Longest Lines
    Go, and add test

--- moderate/longestlines.go
+++ moderate/longestlines.go
@@ -8,33 +8,29 @@ import (
 	"strings"
 )
 
+func longest(p []string, q string) []string {
+	for ix, i := range p {
+		if len(q) > len(i) {
+			p = append(p[:ix], append([]string{q}, p[ix:len(p)-1]...)...)
+			break
+		}
+	}
+	return p
+}
+
 func main() {
+	var n int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer data.Close()
-	reader := bufio.NewReader(data)
-	s, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
+	scanner := bufio.NewScanner(data)
+	scanner.Scan()
+	fmt.Sscan(scanner.Text(), &n)
+	p := make([]string, n)
+	for scanner.Scan() {
+		p = longest(p, scanner.Text())
 	}
-	var n int
-	fmt.Sscan(strings.TrimSpace(s), &n)
-	lol := make([]string, n)
-	for {
-		s, err = reader.ReadString('\n')
-		if err != nil {
-			break
-		}
-		for ix, i := range lol {
-			if len(s) > len(i) {
-				lol = append(lol[:ix], append([]string{s}, lol[ix:n-1]...)...)
-				break
-			}
-		}
-	}
-	for _, i := range lol {
-		fmt.Print(i)
-	}
+	fmt.Println(strings.Join(p, "\n"))
 }
