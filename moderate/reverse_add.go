113a6692343804771c284a0c5d7a6be416592cc4
Nicolas Kaiser <nikai@nikai.net>
2016-08-17 20:15:09 +0200

    refactor Reverse and Add
    Go, and add test and benchmark

--- moderate/reverse_add.go
+++ moderate/reverse_add.go
@@ -7,14 +7,27 @@ import (
 	"os"
 )
 
-func rev(a int) (r int) {
-	for ; a > 0; a /= 10 {
-		r = 10*r + a%10
+func rev(n int) (r int) {
+	for ; n > 0; n /= 10 {
+		r = 10*r + n%10
 	}
 	return r
 }
 
+func revAdd(n int) string {
+	var c, r int
+	for c < 100 {
+		r = rev(n)
+		if r == n {
+			return fmt.Sprint(c, n)
+		}
+		n, c = n+r, c+1
+	}
+	return "not found"
+}
+
 func main() {
+	var n int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -22,19 +35,7 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var a, b, r int
-		fmt.Sscan(scanner.Text(), &a)
-		for b < 100 {
-			r = rev(a)
-			if r == a {
-				break
-			}
-			a, b = a+r, b+1
-		}
-		if b < 100 {
-			fmt.Println(b, a)
-		} else {
-			fmt.Println("not found")
-		}
+		fmt.Sscan(scanner.Text(), &n)
+		fmt.Println(revAdd(n))
 	}
 }
--- moderate/reverse_add_test.go
+++ moderate/reverse_add_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRev(t *testing.T) {
 	for k, v := range map[int]int{
@@ -13,15 +16,54 @@ func TestRev(t *testing.T) {
 	}
 }
 
+func TestRevAdd(t *testing.T) {
+	for k, v := range map[int]string{
+		195:  "4 9339",
+		155:  "3 4444",
+		2139: "2 26862",
+		196:  "not found",
+		0:    "0 0",
+		1:    "0 1",
+		2:    "0 2",
+		3:    "0 3",
+		4:    "0 4",
+		5:    "0 5",
+		9998: "6 8836388",
+		9999: "0 9999"} {
+		if r := revAdd(k); r != v {
+			t.Errorf("failed: revAdd %d is %s, got %s",
+				k, v, r)
+		}
+	}
+}
+
 func BenchmarkRev(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		rev(i % 10000)
 	}
 }
 
-func rev(a int) (r int) {
-	for ; a > 0; a /= 10 {
-		r = 10*r + a%10
+func BenchmarkRevAdd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		revAdd(i % 10000)
+	}
+}
+
+func rev(n int) (r int) {
+	for ; n > 0; n /= 10 {
+		r = 10*r + n%10
 	}
 	return r
+}
+
+func revAdd(n int) string {
+	var c, r int
+	for c < 100 {
+		r = rev(n)
+		if r == n {
+			return fmt.Sprint(c, n)
+		}
+		n, c = n+r, c+1
+	}
+	return "not found"
 }
