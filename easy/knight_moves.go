10d68d17680ecddf9a301c613cd28f7e59e5ca53
Nicolas Kaiser <nikai@nikai.net>
2016-08-20 22:12:15 +0200

    refactor Knight Moves
    Go, and add test and benchmark

--- easy/knight_moves.go
+++ easy/knight_moves.go
@@ -12,6 +12,44 @@ func pos(l, r uint8) string {
 	return string([]byte{'a' + l, '1' + r})
 }
 
+func knight(q string) string {
+	var m []string
+	l, r := q[0]-'a', q[1]-'1'
+	if l > 1 {
+		if r > 0 {
+			m = append(m, pos(l-2, r-1))
+		}
+		if r < 7 {
+			m = append(m, pos(l-2, r+1))
+		}
+	}
+	if l > 0 {
+		if r > 1 {
+			m = append(m, pos(l-1, r-2))
+		}
+		if r < 6 {
+			m = append(m, pos(l-1, r+2))
+		}
+	}
+	if l < 7 {
+		if r > 1 {
+			m = append(m, pos(l+1, r-2))
+		}
+		if r < 6 {
+			m = append(m, pos(l+1, r+2))
+		}
+	}
+	if l < 6 {
+		if r > 0 {
+			m = append(m, pos(l+2, r-1))
+		}
+		if r < 7 {
+			m = append(m, pos(l+2, r+1))
+		}
+	}
+	return strings.Join(m, " ")
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -20,40 +58,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var m []string
-		l, r := scanner.Text()[0]-'a', scanner.Text()[1]-'1'
-		if l > 1 {
-			if r > 0 {
-				m = append(m, pos(l-2, r-1))
-			}
-			if r < 7 {
-				m = append(m, pos(l-2, r+1))
-			}
-		}
-		if l > 0 {
-			if r > 1 {
-				m = append(m, pos(l-1, r-2))
-			}
-			if r < 6 {
-				m = append(m, pos(l-1, r+2))
-			}
-		}
-		if l < 7 {
-			if r > 1 {
-				m = append(m, pos(l+1, r-2))
-			}
-			if r < 6 {
-				m = append(m, pos(l+1, r+2))
-			}
-		}
-		if l < 6 {
-			if r > 0 {
-				m = append(m, pos(l+2, r-1))
-			}
-			if r < 7 {
-				m = append(m, pos(l+2, r+1))
-			}
-		}
-		fmt.Println(strings.Join(m, " "))
+		fmt.Println(knight(scanner.Text()))
 	}
 }
