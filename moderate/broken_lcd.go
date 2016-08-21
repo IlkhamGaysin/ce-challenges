a6c52448330253529e0dd75769d5ac84fb5d009c
Nicolas Kaiser <nikai@nikai.net>
2016-08-21 17:39:34 +0200

    refactor Broken LCD
    Go, and add test

--- moderate/broken_lcd.go
+++ moderate/broken_lcd.go
@@ -24,6 +24,32 @@ func valid(y int, d bool, x int) bool {
 	return y&x == y
 }
 
+func brokenLcd(p, q string) bool {
+	var v bool
+	t := strings.Fields(p)
+	for u := 0; u <= len(t)-len(q)+1; u++ {
+		var j int
+		v = true
+		for i := 0; i < len(q); i++ {
+			d := i < len(q)-1 && q[i+1] == '.'
+			var x int
+			fmt.Sscanf(t[u+j], "%b", &x)
+			if !valid(digits[q[i]], d, x) {
+				v = false
+				break
+			}
+			if d {
+				i++
+			}
+			j++
+		}
+		if v {
+			return true
+		}
+	}
+	return v
+}
+
 func main() {
 	data, err := os.Open(os.Args[1])
 	if err != nil {
@@ -31,31 +57,10 @@ func main() {
 	}
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var v bool
 		s := strings.Split(scanner.Text(), ";")
-		t := strings.Fields(s[0])
-		for u := 0; u <= len(t)-len(s[1])+1; u++ {
-			var j int
-			v = true
-			for i := 0; i < len(s[1]); i++ {
-				d := i < len(s[1])-1 && s[1][i+1] == '.'
-				var x int
-				fmt.Sscanf(t[u+j], "%b", &x)
-				if !valid(digits[s[1][i]], d, x) {
-					v = false
-					break
-				}
-				if d {
-					i++
-				}
-				j++
-			}
-			if v {
-				fmt.Println("1")
-				break
-			}
-		}
-		if !v {
+		if brokenLcd(s[0], s[1]) {
+			fmt.Println("1")
+		} else {
 			fmt.Println("0")
 		}
 	}
