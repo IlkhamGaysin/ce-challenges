ac1f275055d2910c9ea31dc68bc26db3bd6de598
Nicolas Kaiser <nikai@nikai.net>
2016-08-21 18:32:20 +0200

    refactor Find a Square
    Go, and add test

--- moderate/find_square.go
+++ moderate/find_square.go
@@ -18,8 +18,28 @@ func dist2(a, b point) int {
 	return x*x + y*y
 }
 
+func findSquare(q string) bool {
+	var (
+		x, y int
+		d    []int
+	)
+	t := strings.Split(q, ", ")
+	u := make([]point, 4)
+	for ix, i := range t {
+		fmt.Sscanf(i, "(%d,%d)", &x, &y)
+		u[ix] = point{x, y}
+	}
+	for i := 0; i < 3; i++ {
+		for j := i + 1; j < 4; j++ {
+			d = append(d, dist2(u[i], u[j]))
+		}
+	}
+	sort.Ints(d)
+	return d[0] == d[1] && d[0] == d[2] && d[0] == d[3] &&
+		d[4] == d[5] && 2*d[0] == d[4]
+}
+
 func main() {
-	var x, y int
 	data, err := os.Open(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
@@ -27,20 +47,6 @@ func main() {
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
 	for scanner.Scan() {
-		var d []int
-		t := strings.Split(scanner.Text(), ", ")
-		u := make([]point, 4)
-		for ix, i := range t {
-			fmt.Sscanf(i, "(%d,%d)", &x, &y)
-			u[ix] = point{x, y}
-		}
-		for i := 0; i < 3; i++ {
-			for j := i + 1; j < 4; j++ {
-				d = append(d, dist2(u[i], u[j]))
-			}
-		}
-		sort.Ints(d)
-		fmt.Println(d[0] == d[1] && d[0] == d[2] && d[0] == d[3] &&
-			d[4] == d[5] && 2*d[0] == d[4])
+		fmt.Println(findSquare(scanner.Text()))
 	}
 }
--- moderate/find_square_test.go
+++ moderate/find_square_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+)
 
 type tuple struct {
 	x1, y1, x2, y2 int
@@ -18,6 +23,18 @@ func TestDist2(t *testing.T) {
 	}
 }
 
+func TestFindSquare(t *testing.T) {
+	for k, v := range map[string]bool{
+		"(1,6), (6,7), (2,7), (9,1)": false,
+		"(4,1), (3,4), (0,5), (1,2)": false,
+		"(4,6), (5,5), (5,6), (4,5)": true} {
+		if r := findSquare(k); r != v {
+			t.Errorf("failed: findSquare %s is %t, got %t",
+				k, v, r)
+		}
+	}
+}
+
 func BenchmarkDist2(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		dist2(point{i % 11, (i / 11) % 11}, point{(i / 121) % 11, (i / 1331) % 11})
@@ -31,4 +48,25 @@ type point struct {
 func dist2(a, b point) int {
 	x, y := a.x-b.x, a.y-b.y
 	return x*x + y*y
+}
+
+func findSquare(q string) bool {
+	var (
+		x, y int
+		d    []int
+	)
+	t := strings.Split(q, ", ")
+	u := make([]point, 4)
+	for ix, i := range t {
+		fmt.Sscanf(i, "(%d,%d)", &x, &y)
+		u[ix] = point{x, y}
+	}
+	for i := 0; i < 3; i++ {
+		for j := i + 1; j < 4; j++ {
+			d = append(d, dist2(u[i], u[j]))
+		}
+	}
+	sort.Ints(d)
+	return d[0] == d[1] && d[0] == d[2] && d[0] == d[3] &&
+		d[4] == d[5] && 2*d[0] == d[4]
 }
